282a4a8554e374a5fa216ca3724bb3b709a33445
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-02-11 17:45:28 -0600

    FP-165: Add comment to avoid change to base.js

--- taccsite_cms/static/site_cms/scripts/base.js
+++ taccsite_cms/static/site_cms/scripts/base.js
@@ -1,4 +1,17 @@
 window.onload = function() {
+
+  // Load user authentication JSON from Portal, and perform dependent actions
+  // - If user is authenticated:
+  //     - Populate username (to show that user is logged in)
+  //     - Show portal nav dropdown (it is pre-hidden via classname)
+  // - If user is NOT authenticated:
+  //     - Show portal login link (it is pre-hidden via classname)
+  /* NOTE: This file is loaded by this codebase, and by an external codebase:
+    - https://bitbucket.org/taccaci/frontera-tech-docs/src/429f05f23d95196f723c4f5fecd20823ceb73fe3/frontera_theme/base.html
+    - https://gitlab.tacc.utexas.edu/wma-cms/frontera-cms/blob/188cd859a2fe3b37ecae7b6457e06be1b4acd6b2/taccsite_cms/templates/base.html#L67
+  */
+  /* WARNING: Code on this file will run on fronteraweb.tacc.utexas.edu/user-guide/, also. */
+  /* SEE: ___link_coming___ */
   function loadJSON(path, success, error)
   {
       var xhr = new XMLHttpRequest();
@@ -17,7 +30,6 @@ window.onload = function() {
       xhr.open("GET", path, true);
       xhr.send();
   }
-
   loadJSON('/api/users/auth/',
            function(data) { console.log(data);
                jQuery('#frontera-core-username').text(data['username']);
@@ -25,5 +37,5 @@ window.onload = function() {
            },
            function(xhr) { console.error(xhr); }
   );
-  
+
 }
