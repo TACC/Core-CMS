f650c5adbefce643a9b2cf5889ec074203fff37c
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-07-04 17:32:48 -0500

    GL-29 Revert renames of cms_menu templates
    Doing both, together—Git can't track it well—cuz PR'll squash it anyway.

--- taccsite_cms/urls.py
+++ taccsite_cms/urls.py
@@ -21,7 +21,7 @@ if settings.PORTAL:
     from django.views.generic.base import TemplateView
     urlpatterns += [
         # FAQ: Allows direct access to isolated CMS menu markup for the Portal and User Guide to render
-        url(r'^cms/nav/pages/markup/$', TemplateView.as_view(template_name='cms_menu.html'), name='menu_pages_markup'),
+        url(r'^cms/nav/pages/markup/$', TemplateView.as_view(template_name='cms_menu.raw.html'), name='menu_pages_markup'),
     ]
 
 urlpatterns += [
