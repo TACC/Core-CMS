394f4a1893772c7499362f4c0895a23837a53512
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-09-03 14:31:05 -0500

    GL-34: Use if/else

--- taccsite_cms/static/site_cms/scripts/debug.js
+++ taccsite_cms/static/site_cms/scripts/debug.js
@@ -1,12 +1,10 @@
 'use strict';
 
-// In DEBUG mode, notify developer in console
 if ( window.DEBUG === true ) {
+  // In DEBUG mode, notify developer in console
   console.warn('Debug Mode', window.DEBUG);
-}
-
-// In production, prevent output from these `console` methods
-if ( window.DEBUG === false ) {
+} else {
+  // In production, prevent output from these `console` methods
   console.info = function () {};
   console.debug = function () {};
 }
