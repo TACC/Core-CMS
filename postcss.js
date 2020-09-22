68668e11399c1e013e1025dc499c06177a94d516
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-09-22 17:30:48 -0500

    GL-37: Quick: Handle error/output from postcss cmd

--- postcss.js
+++ postcss.js
@@ -7,6 +7,26 @@ const { exec } = require('child_process');
 
 const env = dotenv.config({ path: '.env' }).parsed;
 
+// Isolate boilerplate error/ouput handling logic
+function execCallback(err, stdout, stderr) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  if (stderr) {
+    console.error(stderr);
+    return;
+  }
+  console.log(stdout);
+}
+function parallelCallback(err, results) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(results);
+}
+
 // Process via CLI (`postcss-cli`) via Node (this script)
 // FAQ: The CLI is the easiest and cheapest "PostCSS Runner",
 // SEE: https://github.com/postcss/postcss-cli#readme
@@ -15,16 +35,10 @@ const env = dotenv.config({ path: '.env' }).parsed;
 function buildStylesCore() {
   // Quote globbed paths to prevent OS from parsing them
   // SEE: https://github.com/postcss/postcss-cli/issues/142#issuecomment-310681302
-  exec(`postcss "taccsite_cms/static/site_cms/styles/exports/**/*.css" --base "taccsite_cms/static/site_cms/styles/exports" --dir "taccsite_cms/static/build/styles"`);
+  exec(`postcss "taccsite_cms/static/site_cms/styles/exports/**/*.css" --base "taccsite_cms/static/site_cms/styles/exports" --dir "taccsite_cms/static/build/styles"`, execCallback);
 }
 function buildStylesCustom() {
-  exec(`postcss "taccsite_custom/${env.CUSTOM_ASSET_DIR}/static/${env.CUSTOM_ASSET_DIR}/styles/exports/**/*.css" --base "taccsite_custom/${env.CUSTOM_ASSET_DIR}/static/${env.CUSTOM_ASSET_DIR}/styles/exports" --dir "taccsite_custom/${env.CUSTOM_ASSET_DIR}/static/build/styles"`);
-}
-
-// NOTE: In an external function for legibility (and to avoid lint error)
-function parallelCallback(err, results) {
-  if (err) console.error(err);
-  console.log(results);
+  exec(`postcss "taccsite_custom/${env.CUSTOM_ASSET_DIR}/static/${env.CUSTOM_ASSET_DIR}/styles/exports/**/*.css" --base "taccsite_custom/${env.CUSTOM_ASSET_DIR}/static/${env.CUSTOM_ASSET_DIR}/styles/exports" --dir "taccsite_custom/${env.CUSTOM_ASSET_DIR}/static/build/styles"`, execCallback);
 }
 
 // Build process for styles may be run in parallel because they are independent
