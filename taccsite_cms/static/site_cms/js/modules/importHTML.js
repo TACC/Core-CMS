e8700a5fdf84c29f2350bf7c6995169c18bf0308
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-12-18 15:52:16 -0600

    FP-531: Remove obsolete comments
    (and add missing spaces to a doc block)

--- taccsite_cms/static/site_cms/js/modules/importHTML.js
+++ taccsite_cms/static/site_cms/js/modules/importHTML.js
@@ -1,14 +1,9 @@
 /**
-* Clone markup from an endpoint into a specific element
-*
-* WARNING: Implicit requirements
-* - The specific element must be a `ul` tag
-* - The specific element must have the ID `cms-nav-pages`
-* - The specific element must have the attribute `data-cms-nav-endpoint`
-* @param {HTMLElement} containerElement - The element in which to place the markup
-* @param {string} markupUrl - The URL at which to fetch the markup
-* @param {function} [callback] - A function to run after placing the markup
-* @todo Rewrite function to be a custom element
+ * Clone markup from an endpoint into a specific element
+ * @param {HTMLElement} containerElement - The element in which to place the markup
+ * @param {string} markupUrl - The URL at which to fetch the markup
+ * @param {function} [callback] - A function to run after placing the markup
+ * @todo Rewrite function to be a custom element
 */
 export function fromURL(containerElement, markupUrl, callback) {
   if (markupUrl) {
