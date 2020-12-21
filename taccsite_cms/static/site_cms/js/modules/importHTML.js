3857c5c6b7341a70742055515d2c285df36c23ca
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-12-20 23:34:57 -0600

    FP-531: Allow nav_search.raw to not have wrappers
    - Support two ways to importHTML
    - Support two ways to registerCustomElement
    - Remove <template> wrap and <tacc-search-bar> from nav_search.raw.html
    - Wrap nav_search.raw.html include in <tacc-search-bar> and <template>

--- taccsite_cms/static/site_cms/js/modules/importHTML.js
+++ taccsite_cms/static/site_cms/js/modules/importHTML.js
@@ -1,23 +1,34 @@
 /**
- * Clone markup from an endpoint into a specific element
- * @param {HTMLElement} containerElement - The element in which to place the markup
- * @param {string} markupUrl - The URL at which to fetch the markup
- * @param {function} [callback] - A function to run after placing the markup
- * @todo Rewrite function to be a custom element
-*/
-export function fromURL(containerElement, markupUrl, callback) {
-  if (markupUrl) {
-    fetch(markupUrl).then((response) => {
-      return response.text();
-    }).then((data) => {
-      containerElement.innerHTML = data;
-    }, (err) => {
-      containerElement.innerHTML = null;
+ * Return markup from an endpoint
+ * @param {string} markupURL - The URL from which to get the markup
+ * @returns {Promise<string>} Promise that returns markup if resolved
+ */
+export function getFromURL(markupURL) {
+  if (markupURL) {
+    return fetch(markupURL).then(response => {
+      const markup = response.text();
+      return markup;
+    }).catch(err => {
       console.error(err);
-    }).finally(() => {
-      if (typeof callback === 'function') {
-        callback();
-      }
     });
+  } else {
+    return Promise.reject(new Error('Invalid `markupURL` provided'));
+  }
+}
+
+/**
+ * Insert markup from an endpoint into a specific element
+ * @param {string} markupURL - The URL from which to get the markup
+ * @param {HTMLElement} container - The element into which to place the markup
+ * @returns {Promise<HTMLElement>} Promise that returns the container
+ */
+export function insertFromURL(markupURL, container) {
+  if (markupURL) {
+    return getFromURL(markupURL).then(markup => {
+      container.innerHTML = markup;
+      return container;
+    });
+  } else {
+    return Promise.reject(new Error('Invalid `container` provided'));
   }
 }
--- taccsite_cms/static/site_cms/js/modules/registerCustomElement.js
+++ taccsite_cms/static/site_cms/js/modules/registerCustomElement.js
@@ -1,10 +1,29 @@
 /**
+ * Create an HTML Template element from a given HTML element string
+ * @param {string} name - Name of custom element to register
+ * @param {string} html - HTML for shadow DOM of custom element
+ * @return {Element}
+ */
+export function fromHTMLString(name, html) {
+  let template = document.createElement('template');
+
+  // Never return a text node of whitespace
+  html = html.trim();
+
+  template.innerHTML = html;
+  template = document.head.appendChild(template);
+
+  return fromTemplate(name, template);
+}
+
+/**
  * Populate custom element of givenname with content of a given HTML template
- * @param {String} name - Name of the custom element to register
- * @param {HTMLTemplateElement} template - An HTML template
+ * @param {string} name - Name of custom element to register
+ * @param {HTMLTemplateElement} template - HTML for shadow DOM of custom element
  * @return {Element}
  */
 export function fromTemplate(name, template) {
+
   console.log({template});
 
   window.customElements.define(name, class extends HTMLElement {
@@ -14,7 +33,7 @@ export function fromTemplate(name, template) {
       console.log({content: template.content});
 
       this.attachShadow({ mode: "open" });
-      this.shadowRoot.appendChild(template.content);
+      this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
   });
 }
