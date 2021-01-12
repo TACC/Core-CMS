78c8586ee2f45774d1f229c4606f809167b35554
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-01-12 17:13:33 -0600

    FP-713: WIP

---
+++ taccsite_cms/contrib/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BlockquoteConfig(AppConfig):
+    name = 'blockquote'
--- taccsite_cms/settings.py
+++ taccsite_cms/settings.py
@@ -232,6 +232,7 @@ INSTALLED_APPS = [
     'djangocms_bootstrap4.contrib.bootstrap4_utilities',
     'haystack',
     'taccsite_cms',
+    # 'taccsite_cms.contrib.blockquote',
 ]
 
 # Convert list of paths to list of dotted module names
