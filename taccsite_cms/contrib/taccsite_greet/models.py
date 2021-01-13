3899eee5ee2406743aa0e8d2dd898d8305f8ad28
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-01-13 16:51:57 -0600

    FP-713: Greet: Add model doc and short desc

--- taccsite_cms/contrib/taccsite_greet/models.py
+++ taccsite_cms/contrib/taccsite_greet/models.py
@@ -3,4 +3,11 @@ from cms.models.pluginmodel import CMSPlugin
 from django.db import models
 
 class TaccsiteGreet(CMSPlugin):
+    """
+    Components > "Greet" Plugin (Sample)
+    https://url.to/docs/components/sample/
+    """
     guest_name = models.CharField(max_length=50, default='Guest')
+
+    def get_short_description(self):
+        return 'Hello, […]'
