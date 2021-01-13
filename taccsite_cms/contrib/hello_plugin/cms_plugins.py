fba3ebac609095511154abbc76e70f990572a8a1
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-01-13 11:56:19 -0600

    FP-713: Name plugin. Use single quotes.

--- taccsite_cms/contrib/hello_plugin/cms_plugins.py
+++ taccsite_cms/contrib/hello_plugin/cms_plugins.py
@@ -5,7 +5,8 @@ from django.utils.translation import gettext_lazy as _
 
 @plugin_pool.register_plugin
 class HelloPlugin(CMSPluginBase):
-    module = "TACC"
+    module = 'TACC'
+    name = _('Wes\' Test Plugin "Hello Plugin"')
     model = CMSPlugin
-    render_template = "hello_plugin.html"
+    render_template = 'hello_plugin.html'
     cache = False
