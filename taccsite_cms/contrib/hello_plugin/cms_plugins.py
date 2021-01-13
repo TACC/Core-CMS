8425f7484f5b2b0d6f22dbe896494397f05b4d14
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-01-13 12:29:54 -0600

    FP-713: Add model (provides form in editor UI).

--- taccsite_cms/contrib/hello_plugin/cms_plugins.py
+++ taccsite_cms/contrib/hello_plugin/cms_plugins.py
@@ -3,10 +3,16 @@ from cms.plugin_pool import plugin_pool
 from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import gettext_lazy as _
 
+from .models import Hello
+
 @plugin_pool.register_plugin
 class HelloPlugin(CMSPluginBase):
     module = 'TACC'
+    model = Hello
     name = _('Wes\' Test Plugin "Hello Plugin"')
-    model = CMSPlugin
     render_template = 'hello_plugin.html'
     cache = False
+
+    def render(self, context, instance, placeholder):
+        context = super().render(context, instance, placeholder)
+        return context
--- taccsite_cms/contrib/hello_plugin/models.py
+++ taccsite_cms/contrib/hello_plugin/models.py
@@ -1,3 +1,6 @@
+from cms.models.pluginmodel import CMSPlugin
+
 from django.db import models
 
-# Create your models here.
+class Hello(CMSPlugin):
+    guest_name = models.CharField(max_length=50, default='Guest')
