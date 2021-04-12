7654259a3446c8e52cbd2f89d4b7eddf2f7121b9
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-04-12 18:56:43 -0500

    FP-713: Finish Remove WIP Blockquote Plugin

--- taccsite_cms/settings.py
+++ taccsite_cms/settings.py
@@ -243,7 +243,6 @@ INSTALLED_APPS = [
     # RFE: Extract TACC CMS UI components into pip-installable plugins
     # FAQ: The djangocms_bootstrap4 library can serve as an example
     'taccsite_cms.contrib.taccsite_greet',
-    'taccsite_cms.contrib.taccsite_blockquote',
 ]
 
 # Convert list of paths to list of dotted module names
