e7c2867241ab40b3eeb1205335adade14f1c55ce
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-04-13 11:51:53 -0500

    GH-67: Add field help text and widget edit how-to

--- taccsite_cms/contrib/taccsite_sample/models.py
+++ taccsite_cms/contrib/taccsite_sample/models.py
@@ -12,6 +12,12 @@ class TaccsiteSample(CMSPlugin):
     guest_name = models.CharField(
         max_length=50,
         default=default_name,
+        help_text=f'If user is logged in they are greeted by their name. If not logged in, they are greeted as this value. If this value is blank, they are greeted as "{default_name}".',
+        # To change the widget, a new Form class is required
+        # FAQ: Wesley B searched for hours to find this important information
+        # SEE: http://disq.us/p/210zgp2
+        # SEE: [`TaccsiteSamplePlugin.form`](./cms_plugins.py)
+        # widget=forms.TextInput(attrs={'placeholder': 'Search'}),
         blank=True
     )
 
