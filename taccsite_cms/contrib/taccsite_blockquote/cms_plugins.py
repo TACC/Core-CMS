0260226af608deb05adfc54413b5df10a0c01d9f
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-04-13 15:16:08 -0500

    GH-73: Update blockquote based on GH-67

--- taccsite_cms/contrib/taccsite_blockquote/cms_plugins.py
+++ taccsite_cms/contrib/taccsite_blockquote/cms_plugins.py
@@ -2,8 +2,11 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import gettext_lazy as _
+from django.utils.encoding import force_text
 
 from .models import TaccsiteBlockquote
+
+from .defaults import user_name as default_name
 
 @plugin_pool.register_plugin
 class TaccsiteBlockquotePlugin(CMSPluginBase):
@@ -15,19 +18,31 @@ class TaccsiteBlockquotePlugin(CMSPluginBase):
     model = TaccsiteBlockquote
     name = _('Blockquote')
     render_template = 'blockquote.html'
-    cache = False
 
-    def get_name(self, instance, user):
+    cache = False
+    text_enabled = False
+
+    # FAQ: Sets tooltip of preview of this plugin within a Text plugin
+    def icon_alt(self, instance):
+        super_value = force_text(super().icon_alt(instance))
+        name = self.get_name(instance)
+        return f'Hello, {{{name}}} [{super_value}]'
+
+    # Helpers
+
+    def get_name(self, instance, user=None):
         """Get name of authenticated user or the name for any guest."""
 
-        if user.is_authenticated:
+        if user and user.is_authenticated:
             name = user.first_name + ' ' + user.last_name
-        if False:
-            name = 'Jim'
-        else:
+        elif bool(instance.guest_name):
             name = instance.guest_name
+        else:
+            name = default_name
 
         return name
+
+    # Render
 
     def render(self, context, instance, placeholder):
         context = super().render(context, instance, placeholder)
--- taccsite_cms/contrib/taccsite_blockquote/models.py
+++ taccsite_cms/contrib/taccsite_blockquote/models.py
@@ -2,12 +2,19 @@ from cms.models.pluginmodel import CMSPlugin
 
 from django.db import models
 
+from .defaults import user_name as default_name
+
 class TaccsiteBlockquote(CMSPlugin):
     """
     Components > "Blockquote" Model
     https://confluence.tacc.utexas.edu/x/FIEjCQ
     """
-    guest_name = models.CharField(max_length=50, default='Guest')
+    guest_name = models.CharField(
+        max_length=50,
+        default=default_name,
+        help_text=f'If user is logged in they are greeted by their name. If not logged in, they are greeted as this value. If this value is blank, they are greeted as "{default_name}".',
+        blank=True
+    )
 
     def get_short_description(self):
         return 'Hello, […]'
