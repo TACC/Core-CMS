1971c02d7b5af675ac37225f8ad43d1dacc01959
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-04-12 21:11:50 -0500

    GH-67: Support `icon_alt`

--- taccsite_cms/contrib/taccsite_sample/cms_plugins.py
+++ taccsite_cms/contrib/taccsite_sample/cms_plugins.py
@@ -2,6 +2,7 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import gettext_lazy as _
+from django.utils.encoding import force_text
 
 from .models import TaccsiteSample
 
@@ -17,13 +18,22 @@ class TaccsiteSamplePlugin(CMSPluginBase):
     model = TaccsiteSample
     name = _('Sample (Greet User)')
     render_template = 'sample.html'
+
     cache = False
     text_enabled = True
 
-    def get_name(self, instance, user):
+    # FAQ: Sets tooltip of preview of this plugin within a Text plugin
+    def icon_alt(self, instance):
+        super_value = force_text(super().icon_alt(instance))
+        name = self.get_name(instance)
+        return f'Hello, {{{name}}} [{super_value}]'
+
+    # Helpers
+
+    def get_name(self, instance, user=None):
         """Get name of authenticated user or the name for any guest."""
 
-        if user.is_authenticated:
+        if user and user.is_authenticated:
             name = user.first_name + ' ' + user.last_name
         elif bool(instance.guest_name):
             name = instance.guest_name
@@ -31,6 +41,8 @@ class TaccsiteSamplePlugin(CMSPluginBase):
             name = default_name
 
         return name
+
+    # Render
 
     def render(self, context, instance, placeholder):
         context = super().render(context, instance, placeholder)
