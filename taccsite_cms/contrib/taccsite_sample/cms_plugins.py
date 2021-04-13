727ed4b20bc9eae4c884845563c57bd61ab5c6f8
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-04-13 11:45:03 -0500

    GH-67: Doc some TaccsiteSamplePlugin unused props

--- taccsite_cms/contrib/taccsite_sample/cms_plugins.py
+++ taccsite_cms/contrib/taccsite_sample/cms_plugins.py
@@ -8,6 +8,7 @@ from .models import TaccsiteSample
 
 from .defaults import name as default_name
 
+# SEE: http://docs.django-cms.org/en/release-3.7.x/reference/plugins.html
 @plugin_pool.register_plugin
 class TaccsiteSamplePlugin(CMSPluginBase):
     """
@@ -21,12 +22,22 @@ class TaccsiteSamplePlugin(CMSPluginBase):
 
     cache = False
     text_enabled = True
+    # NOTE: Use case is unclear
+    # admin_preview = True
+    # NOTE: To change for all TACC plugins add taccsite_cms/templates/admin/...
+    # change_form_template = 'templates/plugin_change_form.html'
+    # NOTE: To change field widget and other attribute beyond `models.…Field`
+    # form = TaccsiteSamplePluginForm # TODO: Provide example
 
     # FAQ: Sets tooltip of preview of this plugin within a Text plugin
     def icon_alt(self, instance):
         super_value = force_text(super().icon_alt(instance))
         name = self.get_name(instance)
         return f'Hello, {{{name}}} [{super_value}]'
+    # NOTE: Our previews (see `icon_alt`) are rich and have no icon...
+    # TODO: Confirm whether these are ever necessary
+    # def icon_src(self, instance)
+    # def text_editor_button_icon(...)
 
     # Helpers
 
