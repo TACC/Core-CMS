9ae75aa5bfca8f9ffdd99905caa0e1a5466b9015
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-04-20 23:06:13 -0500

    GH-191: Handle bad theme name error

--- taccsite_cms/static/site_cms/css/src/_data/theme.js
+++ taccsite_cms/static/site_cms/css/src/_data/theme.js
@@ -1,9 +1,31 @@
 const dotenv = require('dotenv');
 
 const env = dotenv.config({ path: '.env' }).parsed;
-const theme = env.THEME || 'default'
-const data = require(`./theme.${theme}.json`);
+const theme = env.THEME || 'default';
 
-console.log(data);
+/**
+ * Perform `require` but on fail:
+ * - execute callback
+ * - exit program (as fatal exception)
+ * @param {string} modulePath - Path to the module to require
+ * @param {function} callback - Callback (returns exception from `require()`)
+ * @return {*} Content of the module (if successfully required)
+ * @see https://stackoverflow.com/a/34005010/11817077
+ */
+function requireOrElse(modulePath, callback) {
+  try {
+    return require(modulePath);
+  }
+  catch (e) {
+    if (typeof callback === 'function') {
+      callback(e);
+    }
+    process.exit(1);
+  }
+}
+
+const data = requireOrElse(`./theme.${theme}.json`, () => {
+  console.error(`Unable to find '${__dirname}/theme.${theme}.json'`);
+});
 
 module.exports = data;
