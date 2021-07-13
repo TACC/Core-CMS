a196076468b5b27d4de02384934019f18cc17e91
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-07-12 19:02:52 -0500

    Revert #257 (GH-73 & GH-66)
    Revert "Merge pull request #257 from TACC/task/GH-73-tacc-blockquote-plugin"
    
    This reverts commit 0a82cd70c6eef78baba15403e300a7bf50191a1f, reversing
    changes made to f48ecd262899e62c0835da23d6c8193941c771c2.

--- taccsite_cms/settings.py
+++ taccsite_cms/settings.py
@@ -241,13 +241,8 @@ INSTALLED_APPS = [
     # TODO: Extract TACC CMS UI components into pip-installable plugins
     # FAQ: The djangocms_bootstrap4 library can serve as an example
     'taccsite_cms.contrib.taccsite_sample',
-    'taccsite_cms.contrib.taccsite_blockquote',
-    'taccsite_cms.contrib.taccsite_offset',
     'taccsite_cms.contrib.taccsite_static_article_list',
     'taccsite_cms.contrib.taccsite_static_article_preview',
-    # Restore djangocms plugins that bootstrap4 hides
-    'taccsite_cms.contrib.bootstrap4_djangocms_link',
-    'taccsite_cms.contrib.bootstrap4_djangocms_picture',
 ]
 
 # Convert list of paths to list of dotted module names
