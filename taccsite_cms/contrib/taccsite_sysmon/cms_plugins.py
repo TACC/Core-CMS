e7785757f78d57f5691860f09138776df09a9c29
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-07-30 17:24:00 -0500

    GH-89: Initial SysMon plugin (functional)

---
+++ taccsite_cms/contrib/taccsite_sysmon/cms_plugins.py
@@ -0,0 +1,52 @@
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from django.utils.translation import gettext_lazy as _
+
+from taccsite_cms.contrib.helpers import concat_classnames
+from taccsite_cms.contrib.taccsite_offset.cms_plugins import get_direction_classname
+
+from .models import TaccsiteSysmon
+
+@plugin_pool.register_plugin
+class TaccsiteSysmonPlugin(CMSPluginBase):
+    """
+    Components > "System Monitor" Plugin
+    https://confluence.tacc.utexas.edu/x/FIEjCQ
+    """
+    module = 'TACC Site'
+    model = TaccsiteSysmon
+    name = _('System Monitor')
+    render_template = 'sysmon.html'
+
+    cache = True
+    text_enabled = True
+    allow_children = False
+
+    fieldsets = [
+        (_('Single System'), {
+            'description': 'Only a single system may be shown. After editing this plugin, reload the page to load system data.',
+            'fields': (
+                'system',
+            )
+        }),
+        (_('Advanced settings'), {
+            'classes': ('collapse',),
+            'fields': (
+                'attributes',
+            )
+        }),
+    ]
+
+    # Render
+
+    def render(self, context, instance, placeholder):
+        context = super().render(context, instance, placeholder)
+        request = context['request']
+
+        classes = concat_classnames([
+            's-sysmon',
+            instance.attributes.get('class'),
+        ])
+        instance.attributes['class'] = classes
+
+        return context
