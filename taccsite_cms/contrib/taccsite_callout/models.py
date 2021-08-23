bba22efdf385d7b4d2c3395f94a87ba11461fefc
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-08-23 13:56:56 -0500

    GH-321: Resize Callout image to parent height

--- taccsite_cms/contrib/taccsite_callout/models.py
+++ taccsite_cms/contrib/taccsite_callout/models.py
@@ -22,6 +22,13 @@ class TaccsiteCallout(CMSPlugin):
         max_length=200,
     )
 
+    resize_figure_to_fit = models.BooleanField(
+        verbose_name=_('Resize any image to fit'),
+        help_text=_('Make image shorter or taller to match the height of text beside it.'),
+        blank=False,
+        default=True
+    )
+
     attributes = fields.AttributesField()
 
     def get_short_description(self):
---
+++ taccsite_cms/static/site_cms/js/modules/elementTransformer.js
@@ -0,0 +1,98 @@
+/**
+ * Resize content to fit parent height
+ *
+ * - Manipulates attributes within existing markup.
+ * - Size is NOT dynamically updated after initial load.
+ * @module elementTransformer
+ */
+
+/** Resize first child element to match parent height (track state in markup) */
+export class SizeContentToFit {
+
+
+
+  // FAQ: Offers clarity of intent and consistency with state attr.
+  // NOTE: Could faciliate programmatic transforms (like as a plugin option)
+  /** A suggested selector to match the container */
+  static containerSelector = '[data-transform="size-content-to-fit"]';
+
+
+
+  /**
+   * Initialize and resize
+   * @param {HTMLElement} container - The direct parent of the content to resize
+   */
+  constructor (container) {
+    /** The `HTMLElement` containing the content to resize */
+    this.container = container;
+    /** The `HTMLElement` to resize */
+    this.content = container.querySelector(':scope > *');
+
+    // GH-320: Test whether `this.content` was in the DOM at runtime
+    // FAQ: Use `cloneNode` to NOT watch element reference that is updated later
+    // console.log({
+    //   container: this.container.cloneNode(true),
+    //   content: this.content.cloneNode(true)
+    // });
+
+    this.resizeContent();
+  }
+
+
+
+  /** Mark transformation as in the given state */
+  setState(state) {
+    this.container.dataset.transformState = state;
+  }
+
+  /** Mark transformation as NOT in the given state */
+  removeState(state) {
+    // NOTE: Multiple states are not supported, so there is no use for `state`
+    this.container.dataset.transformState = null;
+  }
+
+  /** Whether transformation is in given state */
+  isState(state) {
+    return (this.container.dataset.transformState == state);
+  }
+
+  /** Whether to resize the content */
+  shouldResizeContent() {
+    if (this.container.getAttribute('hidden') !== null) {
+      this.content.style.offsetHeight = '0';
+      this.container.removeAttribute('hidden');
+    }
+  }
+
+
+  /** Resize the content */
+  resizeContent() {
+    /* To prevent natural height of content from increasing container height */
+    /* FAQ: Module will set wrong height if content is taller than is desired */
+    if (this.container.getAttribute('hidden') !== null) {
+      this.content.style.height = '0';
+      this.container.removeAttribute('hidden');
+    }
+
+    /* To inform observers that this module is active */
+    this.setState('resizing-content');
+
+    /* To make container (and its content) the same height as a root element */
+    /* FAQ: Given tall content, figure height equals content height */
+    /* FAQ: Given hidden content, figure height equals desired content height */
+    this.container.style.height = '100%';
+    this.content.style.height = this.container.offsetHeight + 'px';
+    this.container.style.height = null;
+
+    /* To clean up mess (only if it appears to be the mess of this module) */
+    if (this.isState('resizing-content')) {
+      this.removeState('resizing-content');
+      if ( ! this.container.getAttribute('style')) {
+        this.container.removeAttribute('style');
+      }
+    }
+
+    /* To inform observers that this module is done */
+    this.setState('complete');
+  }
+}
