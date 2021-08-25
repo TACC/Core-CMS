b57e072b16b46a040a515b8501bb908f301ad491
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-08-25 10:43:42 -0500

    GH-88: Do not allow Bootstrap Picture

--- taccsite_cms/contrib/taccsite_system_specs/cms_plugins.py
+++ taccsite_cms/contrib/taccsite_system_specs/cms_plugins.py
@@ -28,7 +28,6 @@ class TaccsiteSystemSpecsPlugin(LinkPlugin):
     child_classes = [
         'TaccsiteSystemMonitorPlugin',
         'PicturePlugin',
-        'Bootstrap4PicturePlugin',
         'TaccsiteDataListPlugin',
     ]
 
@@ -107,8 +106,7 @@ class TaccsiteSystemSpecsPlugin(LinkPlugin):
         for plugin_instance in instance.child_plugin_instances:
             if (type(plugin_instance).__name__ == 'TaccsiteSystemMonitor'):
                 context.update({ 'sysmon_plugin': plugin_instance })
-            if (type(plugin_instance).__name__ == 'Picture' or
-                type(plugin_instance).__name__ == 'Bootstrap4Picture'):
+            if (type(plugin_instance).__name__ == 'Picture'):
                 context.update({ 'image_plugin': plugin_instance })
             if (type(plugin_instance).__name__ == 'TaccsiteDataList'):
                 context.update({ 'data_plugin': plugin_instance })
