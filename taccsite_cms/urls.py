78753bf57d357e7e887ccf1eed40ed38102c772f
Alex Rocha <rochaa@tacc.utexas.edu>
2021-09-10 17:02:27 -0500

    Adding support for CMS auth via core portal

--- taccsite_cms/urls.py
+++ taccsite_cms/urls.py
@@ -6,13 +6,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views
 from django.contrib.sitemaps.views import sitemap
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.static import serve
+from taccsite_cms import remote_cms_auth as remote_cms_auth
 
 from django.http import request
 from django.views.generic.base import RedirectView
-
 admin.autodiscover()
 
 urlpatterns = [
@@ -20,9 +21,11 @@ urlpatterns = [
         {'sitemaps': {'cmspages': CMSSitemap}}),
 
     url(r'^admin/', admin.site.urls),  # NOQA
+    url(r'^admin/', admin.site.urls),  # NOQA
+    url(r'^cms/logout/', views.LogoutView.as_view(), name='logout'),
 ]
 
-if settings.PORTAL:
+if getattr(settings, 'INCLUDES_CORE_PORTAL', True):
     from django.views.generic.base import TemplateView
     urlpatterns += [
         # FAQ: Allow direct access to markup for Portal and User Guide to render
@@ -30,18 +33,11 @@ if settings.PORTAL:
         url(r'^cms/nav/pages/markup/$', TemplateView.as_view(template_name='nav_cms.raw.html'), name='menu_pages_markup'),
         url(r'^cms/header/branding/markup/$', TemplateView.as_view(template_name='header_branding.html'), name='header_branding_markup'),
         url(r'^cms/header/logo/markup/$', TemplateView.as_view(template_name='header_logo.html'), name='header_logo_markup'),
-    ]
-
-if settings.FEATURES['blog']:
-    urlpatterns += [
-        # Support `taggit_autosuggest` (from `djangocms-blog`)
-        url(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
+        url(r'^remote/login/$', remote_cms_auth.verify_and_auth, name='verify_and_auth'),
     ]
 
 urlpatterns += [
     url(r'^', include('cms.urls')),
-    # url(r'^$', RedirectView.as_view(url=request.build_absolute_uri('/')), name='home')
-    # url(r'^', include('djangocms_forms.urls')), # FP-416: Pending full support
 ]
 
 # This is only needed when using runserver.
