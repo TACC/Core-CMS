0c8662f22a6bb50cc5ca31b3e412b2cc322a66ba
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-03-09 10:53:09 -0600

    Quick: Polish settings_custom.example.py

--- taccsite_cms/settings_custom.example.py
+++ taccsite_cms/settings_custom.example.py
@@ -1,43 +1,131 @@
+'''
+A `settings_custom.py` file can override default values in settings.py
 
+The file is loaded after default settings but before settings assignment is complete, so we can override settings in:
+- _either_ `settings_custom.py` (usually set in custom sites)
+- _or_ in `settings_local.py` (usually used in a local dev environment)
+
+To override a setting:
+1. Copy/Paste the default settings.
+2. Set the new/custom values.
+
+If a setting override is for a custom CMS Project, the change is made in the `settings_custom.py` file of that project's directory and (if not a secret) can be committed.
+
+Unless modifying default behavior for the CMS Core (and thus all custom Projects) (which probably means that setting should be in the `settings.py` file), the `settings_custom.py` file that should be modified is the one in the appropriate CMS Project directory.
 '''
 
-settings_custom.py file can be used to override default values in settings.py
+########################
+# WALKTHROUGH
+########################
 
-The settings_custom.py file is loaded after default settings but before settings assignment is complete,
-giving us the opportunity to override settings in either settings_custom.py (usually set in custom sites)
-or in settings_local.py (usually used in a local dev environment).
-
-To override a setting,
-simply copy/paste the default settings and set the new/custom values
-
-If a setting override is for a custom site in an existing submodule, the change is made in
-the settings_custom.py file of that submodules site directory and (if not a secret) can be committed
-
-
-Unless modifying default behavior for the default cms and all custom sites (which probably means that
-setting should be in the settings.py file), the settings_custom.py file that should be modified is
-the one in the appropriate taccsite_custom site directory.
-
-'''
-
-# For example if we want to change LDAP auth settings for a custome site, let's say frontera-cms
-# we simply copy the setting from settings.py, and
-# assign the new value in "Core-CMS/taccsite_custom/frontera-cms/settings_custom.py"
-''' Example LDAP Auth Setting change where we just changing the ldap url '''
+# To change LDAP auth settings for a custom CMS Project (e.g. `frontera-cms`):
+# 1. Copy the setting from `settings.py`
+# 2. Assign the new value in `Core-CMS/taccsite_custom/frontera-cms/settings_custom.py`.
 AUTH_LDAP_SERVER_URI = "ldap://cluster.ldap.tacc.utexas.edu"
 
-#The same goes for other more commonly customized values like below
-# A customization to this default would be applied in settings_custom.py of the appropriate custom site
-PORTAL_LOGO = [
-    "portal",
-    "site_cms/img/org_logos/portal.png",
+# The same goes for other more commonly customized values like below.
+
+########################
+# DJANGO CMS SETTINGS
+########################
+
+CMS_TEMPLATES = (
+    ('standard.html', 'Standard'),
+    ('fullwidth.html', 'Full Width'),
+
+    ('home_portal.html', 'Standard Portal Homepage'),
+
+    ('guide.html', 'Guide'),
+    ('guides/getting_started.html', 'Guide: Getting Started'),
+    ('guides/data_transfer.html', 'Guide: Data Transfer'),
+    ('guides/data_transfer.globus.html', 'Guide: Globus Data Transfer'),
+    ('guides/portal_technology.html', 'Guide: Portal Technology Stack'),
+)
+
+########################
+# BRANDING
+########################
+
+_CUSTOM_BRANDING = [
+    "sgci",
+    "brainmap-cms/img/org_logos/sgci-logo-sans-text.svg",
+    "branding-logo--tall",
+    "https://sciencegateways.org/",
+    "_blank",
+    "SGCI Logo",
+    "anonymous",
+    "True",
+]
+
+BRANDING = [ TACC_BRANDING, UTEXAS_BRANDING, NSF_BRANDING, _CUSTOM_BRANDING ]
+
+########################
+# LOGOS
+########################
+
+LOGO =  [
+    "brainmap",
+    "brainmap-cms/img/org_logos/brainmap-logo.png",
     "",
-    "/",
+    "https://brainmap.org",
     "_self",
-    "Portal Logo",
+    "BrainMap Logo",
     "anonymous",
     "True"
 ]
 
-LOGO = PORTAL_LOGO
+########################
+# FAVICON
+########################
 
+FAVICON = {
+    "img_file_src": "brainmap-cms/img/org_logos/favicon.ico"
+}
+
+########################
+# BLOG & SOCIAL METADATA
+########################
+
+# Install required apps
+INSTALLED_APPS += [
+    # Blog/News
+    # 'filer',              # already added
+    # 'easy_thumbnails',    # already added
+    'parler',
+    'taggit',
+    'taggit_autosuggest',
+    'meta',                 # also supports `djangocms_page_meta`
+    'sortedm2m',
+    'djangocms_blog',
+
+    # Metadata
+    'djangocms_page_meta',
+]
+# CAVEAT: 'taggit_autosuggest' requires the following is added to `urls.py`
+"""
+urlpatterns += [
+    # Support `taggit_autosuggest` (from `djangocms-blog`)
+    url(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
+]
+"""
+
+# Metadata: Configure
+META_SITE_PROTOCOL = 'http'
+META_USE_SITES = True
+META_USE_OG_PROPERTIES = True
+META_USE_TWITTER_PROPERTIES = True
+META_USE_GOOGLEPLUS_PROPERTIES = True # django-meta 1.x+
+# META_USE_SCHEMAORG_PROPERTIES=True  # django-meta 2.x+
+
+# Blog/News: Set custom paths for templates
+BLOG_PLUGIN_TEMPLATE_FOLDERS = (
+    ('plugins/default', 'Default template'),    # i.e. `templates/djangocms_blog/plugins/default/`
+    ('plugins/default-clone', 'Clone of default template'),  # i.e. `templates/djangocms_blog/plugins/default-clone/`
+)
+
+# Blog/News: Change default values for the auto-setup of one `BlogConfig`
+# SEE: https://github.com/nephila/djangocms-blog/issues/629
+BLOG_AUTO_SETUP = True
+BLOG_AUTO_HOME_TITLE ='Home'
+BLOG_AUTO_BLOG_TITLE = 'News'
+BLOG_AUTO_APP_TITLE = 'News'
