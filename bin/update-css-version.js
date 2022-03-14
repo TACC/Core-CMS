e8aacfac31570fce474600927dc74c1a7a183024
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-03-14 13:40:53 -0500

    Core-Styles: Plan B: Fix 3.3 Git Rev Build Fail

--- bin/update-css-version.js
+++ bin/update-css-version.js
@@ -3,14 +3,22 @@
 /** Create CSS file to import that prints project version */
 
 const fs = require('fs');
+const childProcess = require('child_process');
 
 const root = __dirname;
 const outFile = root + '/../taccsite_cms/static/site_cms/css/src/_version.css';
 
-const ver = process.env.npm_package_version;
-const rev = getGitRev().substring(0, 7);
 
-const output = `/*! @tacc/core-cms#${rev} (≥ v${ver}) | MIT License | github.com/TACC/Core-CMS */`;
+/**
+ * Whether a Git revision look like something we can use
+ * @param {string} rev - The revision to check
+ * @return {boolean}
+ */
+ function isGitRevOkay(rev) {
+  const isOkay = /[\da-z]+/.test(rev);
+
+  return isOkay;
+}
 
 /**
  * Get the Git revision of the current working directory code
@@ -18,16 +26,29 @@ const output = `/*! @tacc/core-cms#${rev} (≥ v${ver}) | MIT License | github.c
  * @see https://stackoverflow.com/a/34518749/11817077
  */
 function getGitRev() {
-  let rev = fs.readFileSync('.git/HEAD').toString().trim();
-  const revFile = '.git/' + rev.substring(5);
+  const rev = childProcess.execSync('git rev-parse HEAD').toString();
+  const isRevOkay = isGitRevOkay(rev);
 
-  if (rev.indexOf(':') !== -1) {
-    console.log('Reading Git revision from: ' + revFile);
-    rev = fs.readFileSync(revFile).toString().trim();
+  if ( ! isRevOkay) {
+    console.warn(`Revision looks odd. Is this okay?` + rev);
   }
 
   return rev;
 }
 
-console.log(`Updating CSS version to package version ${ver} and Git revision ${rev}[...]`);
-fs.writeFileSync(outFile, output, 'utf8');
+/**
+ * Get data and write content to version file
+ * @return {string}
+ * @see https://stackoverflow.com/a/34518749/11817077
+ */
+(async function writeRevToFile() {
+  // FP-1544: IF tags are annotated THEN replace ver + rev with `git describe`
+  const ver = process.env.npm_package_version;
+  const rev = await getGitRev().substring(0, 7);
+  const cssVersion = `#${rev} (≥ v${ver})`;
+  const output = `/*! @tacc/core-styles${cssVersion} | MIT License | github.com/TACC/Core-Styles */` + "\n";
+
+  console.log(`Updating CSS version to ${cssVersion}`);
+
+  fs.writeFileSync(outFile, output, 'utf8');
+})();
