ee0b1b9e028352bcfeb98dc3e2ae0614d3d1d3d5
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-03-27 02:10:44 -0500

    chore(core-styles): clean up build script

--- bin/build-css-via-cli.js
+++ bin/build-css-via-cli.js
@@ -12,7 +12,7 @@ const ARGS = mininmist(process.argv.slice(2));
 const PROJECT_NAME = ARGS['project'] || '';
 const BUILD_ID = ARGS['build-id'] || '';
 
-/** Execute command to build CSS for Core and optional project/directory */
+/** Build stylesheets for Core and (optional) project */
 (() => {
   const corePath = _getPath('taccsite_cms', 'site_cms');
   const projectPath = _getPath('taccsite_custom/' + PROJECT_NAME, PROJECT_NAME);
@@ -23,32 +23,33 @@ const BUILD_ID = ARGS['build-id'] || '';
   ] : [
     `${ROOT}/${corePath}/.postcssrc.yml`
   ];
-  const configs = '"' + configPaths.join('" "') + '"';
 
-  // To illustrate Project is built on top of Core:
-  // // build Core first
-  console.log(`Building Core styles:`);
-  cmd.runSync(`
-    core-styles build\
-    --input-dir "${ROOT}/${corePath}/src"\
-    --output-dir "${ROOT}/${corePath}/build"\
-    --custom-configs ${configs}\
-    --build-id "${BUILD_ID}"\
-    --verbose\
-  `);
-  // // build Project next (if at all)
+  _build('Core', corePath, configPaths, BUILD_ID);
   if (hasProject) {
-    console.log(`Building "${PROJECT_NAME}" styles:`);
-    cmd.runSync(`
-      core-styles build\
-      --input-dir "${ROOT}/${projectPath}/src"\
-      --output-dir "${ROOT}/${projectPath}/build"\
-      --custom-configs ${configs}\
-      --build-id "${BUILD_ID}"\
-      --verbose\
-    `);
+    _build(PROJECT_NAME, projectPath, configPaths, BUILD_ID);
   }
 })();
+
+/**
+ * Execute command to build stylesheets
+ * @param {string} name - The name of the project
+ * @param {string} path - The path to the project source CSS
+ * @param {array.string} configs - The list of config file paths
+ * @param {string} id - The value to identify the build
+ */
+ function _build(name, path, configs, id) {
+  const configValues = '"' + configs.join('" "') + '"';
+
+  console.log(`Building "${name}" styles:`);
+  cmd.runSync(`
+    core-styles build\
+    --input-dir "${ROOT}/${path}/src"\
+    --output-dir "${ROOT}/${path}/build"\
+    --custom-configs ${configValues}\
+    --build-id "${id}"\
+    --verbose\
+  `);
+}
 
 /**
  * Get path to CSS resources
@@ -56,6 +57,6 @@ const BUILD_ID = ARGS['build-id'] || '';
  * @param {string} [subDirName=dirName] - The name of the sub-directory
  * @return {string}
  */
-function _getPath( dirName, subDirName ) {
+ function _getPath( dirName, subDirName ) {
   return dirName + '/static/' + (subDirName || dirName) + '/css';
 }
