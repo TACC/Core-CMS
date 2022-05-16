41fc8f61ce94e44cc29bba6ed4fb63e7b89b1666
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-05-16 15:03:54 -0500

    feat(fp-1439): get_url_match template filter

---
+++ taccsite_cms/templatetags/get_url_match.py
@@ -0,0 +1,36 @@
+from django import template
+from urllib.parse import urlparse
+
+register = template.Library()
+
+@register.filter(takes_context=True)
+def get_url_match(context, pattern):
+    """
+    Custom Template Filter `get_url_match`
+
+    Use: Render string from URI that matches given pattern.
+
+    Load custom filter into template:
+        {% load get_url_match %}
+
+    Template inline usage:
+        {# given path '.../2020/...' (does something) #}
+        {% if path|get_url_match:"/20\d\d/" %}
+            # do something
+        {% endif %}
+
+        {# given path '.../1921/...' (does nothing) #}
+        {% if path|get_url_match:"/20\d\d/" %}
+            # do something
+        {% endif %}
+    """
+    request = context['request']
+    req_uri = request.build_absolute_uri('/')
+    req_url = urlparse(req_uri)
+    req_path = req.path
+
+    match = re.search(pattern, req_path)
+    if match:
+        return match[0]
+    else:
+        return False
