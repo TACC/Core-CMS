8ff318564512f1384977f6f312d3645c34736c3e
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-07-17 15:23:21 -0500

    chore(ui): polish config file

--- fractal.config.js
+++ fractal.config.js
@@ -4,33 +4,34 @@ const path = require('path');
 const mandelbrot = require('@frctl/mandelbrot');
 const minimist = require('minimist');
 
-const { getStaticFilePath } = require( __dirname + '/bin/get-path.js');
+// Get base config
+const fractal = require('@tacc/core-styles/fractal.config.js');
+const themeConfig = require('@tacc/core-styles/fractal.theme.js');
 
+// Get project
+// (name)
+const { getStaticFilePath } = require( __dirname + '/bin/get-path.js');
 const args = minimist( process.argv.slice( 2 ));
 let projectName = args['project'] || '';
     projectName = ( projectName !== 'core-cms' ) ? projectName : '';
+// (stylesheet)
 const projectCSSFile = projectName
   ? path.join( '/', getStaticFilePath( projectName, 'css/build/site.css'))
   : null;
 
-const fractal = require('@tacc/core-styles/fractal.config.js');
-const themeConfig = require('@tacc/core-styles/fractal.theme.js');
-
-const theme = mandelbrot( Object.assign( themeConfig, {
-  skin: Object.assign( themeConfig.skin, {
-    links: '#877453',
-  })
-}));
-
+// Get base path
 const coreStylesRoot = path.join(
   path.dirname( require.resolve('@tacc/core-styles')),
-  // The '../' exits 'src/' which require.resolve returns a file from
+  // The '../' exits 'src/' which is at the end of what require.resolve returns
   '../'
 );
 
+// Set source paths
+// (for components)
 fractal.components.set('path',
   path.join( coreStylesRoot, 'src/lib/_imports')
 );
+// (for stylesheets)
 fractal.components.set('default.context', {
   styles: {
     external: {
@@ -47,11 +48,17 @@ if ( projectCSSFile ) {
   fractal.cli.log(`+ Included CSS for "${projectName}": '${projectCSSFile}'`);
 }
 
-fractal.web.set('static.path',
-  path.join( coreStylesRoot, 'dist')
-);
+// Set website paths
+fractal.web.set('static.path', path.join( coreStylesRoot, 'dist'));
 fractal.web.set('builder.dest', __dirname + '/taccsite_ui');
 
+// Customize theme
+const theme = mandelbrot( Object.assign( themeConfig, {
+  skin: Object.assign( themeConfig.skin, {
+    links: '#877453',
+  })
+}));
 fractal.web.theme( theme );
 
+// Export
 module.exports = fractal;
