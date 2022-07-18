0e841bf8d5fec7de5e0443cdd393960fa5a7c1b6
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-07-17 20:25:16 -0500

    fix(ui): skip base component css because site.css
    Skip loading base component stylsheets because we load site.css.
    
    Includes new core-styles version, whcih adds shouldSkipBase prop.

--- fractal.config.js
+++ fractal.config.js
@@ -34,6 +34,7 @@ fractal.components.set('path',
 // (for stylesheets)
 fractal.components.set('default.context', {
   styles: {
+    shouldSkipBase: true, // true, because site.css includes components
     external: {
       global: [
         'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
