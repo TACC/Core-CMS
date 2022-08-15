65f23909194390879ac02bd38e076fc0dd50d414
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-08-15 11:57:21 -0500

    feat(settings): fp-1736 new email conf, notes only

--- taccsite_cms/_settings/email.py
+++ taccsite_cms/_settings/email.py
@@ -2,19 +2,17 @@
 
 ########################
 # DJANGO
+# https://docs.djangoproject.com/en/2.2/topics/email/
 ########################
 
-# Development
-# https://docs.djangoproject.com/en/2.1/topics/email/#configuring-email-for-development
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
-DEFAULT_FROM_EMAIL = 'no-reply@localhost'
+# Development, Staging, Production
+# https://confluence.tacc.utexas.edu/x/coR9E
+# EMAIL_HOST = "relay.tacc.utexas.edu"
+# DEFAULT_FROM_EMAIL = 'no-reply@...' # where ... is project's production domain
 
-# Production
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = '?'
-# EMAIL_PORT = 25
-# EMAIL_HOST_USER = '?'
-# EMAIL_HOST_PASSWORD = '?'
-# DEFAULT_FROM_EMAIL = 'no-reply@...'
+# Debugging
+# https://docs.djangoproject.com/en/2.1/topics/email/#configuring-email-for-development
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_HOST = 'localhost'
+# EMAIL_PORT = 1025
+# DEFAULT_FROM_EMAIL = 'no-reply@localhost'
