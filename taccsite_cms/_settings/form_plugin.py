6539485beb1a6df35d9045bb283639aad427f3ab
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-08-15 12:33:53 -0500

    feat(settings): fp-1736 to use default no-reply
    Add note so admins no how to use default no-reply address for a server.

--- taccsite_cms/_settings/form_plugin.py
+++ taccsite_cms/_settings/form_plugin.py
@@ -75,7 +75,7 @@ DJANGOCMS_FORMS_FIELDSETS = (
     (
         _('Submission settings'),
         {
-            'description': 'You can choose to set from which, and to which, e-mail address to send form submissions and/or to save submitted form data in the CMS database',
+            'description': 'You can choose to set from which, and to which, e-mail address to send form submissions and/or to save submitted form data in the CMS database. <small>The "SENDER_EMAIL_ADDRESS" may remain blank, so server default no-reply address would be used.</small>',
             'fields': (
                 'email_to',
                 'email_from',
