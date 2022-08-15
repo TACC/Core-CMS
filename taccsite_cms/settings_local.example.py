faefd66e952169ac03bd5860dca81c619e770e13
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2022-08-15 15:54:09 -0500

    docs: improve a sentence in settings_local example

--- taccsite_cms/settings_local.example.py
+++ taccsite_cms/settings_local.example.py
@@ -1,7 +1,7 @@
 '''
 A `settings_local.py` file can override default values in `settings.py` and `settings_custom.py`.
 
-For more details about overriding settings, see `settings_custom.example.py`.
+For a detailed walkthrough on overriding settings, see `settings_custom.example.py`.
 '''
 
 # Hide error about using Google Recaptcha test keys
