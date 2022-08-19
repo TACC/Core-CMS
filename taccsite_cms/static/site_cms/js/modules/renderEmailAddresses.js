7ef6f005f0f4e8cd82499f4733aa4f683841b0b6
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-08-19 17:36:01 -0500

    feat: ecep-214 renderEmailAddresses.js (tested)
    Loads for every site as delayed script.

--- taccsite_cms/static/site_cms/js/modules/renderEmailAddresses.js
+++ taccsite_cms/static/site_cms/js/modules/renderEmailAddresses.js
@@ -1,38 +1,65 @@
 /**
- * Convert data-attribute-name to dataset.propName
- * @param {string} attr
- * @returns {string}
+ * Names of the URL search parameter and the input field
+ * @typedef {Object} AttrNames
+ * @property {string} [host] - Which attr. has host
+ * @property {string} [domain] - Which attr. has domain
+ * @property {string} [subject] - Which attr. has subject
+ * @property {string} [body] - Which attr. has body
  */
- function _dataAttrFromDatasetProp(attr) {
-  const datasetPropName = attr.replace(/-[a-z]/g, match => match.toUpperCase());
 
-  return datasetPropName;
+/**
+ * The default names for attributes
+ * @enum {string}
+ */
+const ATTRIBUTE_NAMES = {
+  host: 'data-host',
+  domain: 'data-domain',
+  subject: 'data-subject',
+  body: 'data-body',
+};
+
+/**
+ * Update link href from `[data-email="…"]` to `href="mailto:…"`
+ * @param {HTMLElement} element - The link element to update
+ * @param {AttrNames} attributes - The names of attributes with e-mail data
+ */
+function _updateHref(element, attributes) {
+  const host = element.getAttribute(attributes.host);
+  const domain = element.getAttribute(attributes.domain);
+  const subject = element.getAttribute(attributes.subject);
+  const body = element.getAttribute(attributes.body);
+
+  element.href = 'mailto:' + host + '@' + domain;
+  if (subject || body) {
+    element.href += '?';
+  }
+  if (subject) {
+    element.href += 'subject=' + subject;
+  }
+  if (subject && body) {
+    element.href += '&';
+  }
+  if (body) {
+    element.href += 'body=' + body;
+  }
 }
 
 /**
  * Update hyperlinks from `[data-email="…"]` to `href="mailto:…"`
- * @param {string} [hostAttrName='data-host'] - Which attribute has host
- * @param {string} [domainAttrName='data-domain'] - Which attribute has domain
- * @param {HTMLElement} [scopeElement=document] - Element within which to search for link
+ * @param {HTMLElement} [scopeElement=document] - Element within which to search for links
+ * @param {AttrNames} attributes - The names of attributes with e-mail data
  * @return {boolean}
-*/
-export default function renderEmailAddresses({
-  hostAttrName = 'data-host',
-  domainAttrName = 'data-domain',
-  scopeElement = document
-} = {}) {
-  const querySelector = '[' + hostAttrName + '][' + domainAttrName + ']';
+ */
+export default function renderEmailAddresses(
+  scopeElement = document,
+  attributes
+) {
+  const selector = '[' + attributes.host + '][' + attributes.domain + ']';
+  const attributesMerged = Object.assign(attributes, ATTRIBUTE_NAMES);
 
-  scopeElement.querySelector(querySelector).addEventListener('click', event => {
-    event.preventDefault();
+  scopeElement.querySelectorAll(selector).forEach(linkEl => {
+    const updateHref = _updateHref.bind(this, linkEl, attributesMerged);
 
-    const linkEl = event.target;
-    const hostPropName = _dataAttrFromDatasetProp(hostAttrName);
-    const domainPropName = _dataAttrFromDatasetProp(domainAttrName);
-
-    linkEl.href = 'mailto:'
-    + linkEl.dataset[hostPropName]
-    + '@'
-    + linkEl.dataset[domainPropName];
+    linkEl.addEventListener('click', updateHref, {once: true});
   });
 }
