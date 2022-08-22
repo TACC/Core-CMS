b8b4ed2fa2706405cbea726f77696163e807e518
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-08-21 22:37:05 -0500

    feat(js): ecep-214 email links "version 2.0"
    - isolate e-mail building
    - isolate query building
    - remove fake text if no user & domain attr's
    - select fake-text–only e-mails
    - select body-and/or-subject–only e-mails
    - polish jsdoc comments

--- taccsite_cms/static/site_cms/js/modules/updateEmailLinkHrefs.js
+++ taccsite_cms/static/site_cms/js/modules/updateEmailLinkHrefs.js
@@ -19,46 +19,96 @@ const ATTRIBUTE_NAMES = {
 };
 
 /**
+ * The default value for false text manually added into address
+ * @const {string}
+ */
+const FAKE_TEXT = '__REMOVE_THIS__';
+
+/**
+ * Get the e-mail based on available data
+ * @param {string} options
+ * @param {string} [options.href]
+ * @param {string} [options.user]
+ * @param {string} [options.domain]
+ * @param {string} [options.fakeText] - False text manually added into address
+ */
+function _getEmail({href, fakeText, user, domain} = {}) {
+  const emailOld = href.replace('mailto:', '');
+  const emailFix = emailOld.replace(fakeText, '');
+  const emailNew = (user && domain) ? user + '@' + domain : undefined;
+  const email = emailNew || emailFix || emailOld;
+
+  // console.debug({emailOld, emailFix, emailNew, email});
+
+  return email;
+}
+
+/**
+ * Create query string based on available data
+ * @param {string} options
+ * @param {string} [options.subject]
+ * @param {string} [options.body]
+ */
+function _createQuery({subject, body} = {}) {
+  let query = '';
+
+  if (subject || body) {
+    query += '?';
+  }
+  if (subject) {
+    query += 'subject=' + subject;
+  }
+  if (subject && body) {
+    query += '&';
+  }
+  if (body) {
+    query += 'body=' + body;
+  }
+
+  return query;
+}
+
+/**
  * Update `href` attribute, of one e-mail link, based on data attributes
  * @param {HTMLElement} element - The link element to update
- * @param {AttrNames} attributes - The names of attributes storing e-mail data
+ * @param {AttrNames} [attributes] - The names of attributes storing e-mail data
+ * @param {string} [fakeText] - False text manually added into address
  */
-function _updateHref(element, attributes) {
+function _updateHref(element, attributes, fakeText) {
   const user = element.getAttribute(attributes.user);
   const domain = element.getAttribute(attributes.domain);
   const subject = element.getAttribute(attributes.subject);
   const body = element.getAttribute(attributes.body);
 
-  element.href = 'mailto:' + user + '@' + domain;
-  if (subject || body) {
-    element.href += '?';
-  }
-  if (subject) {
-    element.href += 'subject=' + subject;
-  }
-  if (subject && body) {
-    element.href += '&';
-  }
-  if (body) {
-    element.href += 'body=' + body;
-  }
+  const email = _getEmail({href: element.href, user, domain, fakeText});
+  const query = _createQuery({body, subject});
+
+  element.href = 'mailto:' + email + query;
 }
 
 /**
  * Update `href` attribute, of all e-mail links, based on data attributes
  * @param {HTMLElement} [scopeElement=document] - Element within which to search for links
- * @param {AttrNames} attributes - The names of attributes with e-mail data
- * @return {boolean}
+ * @param {AttrNames} [attributes] - The names of attributes with e-mail data
+ * @param {string} [fakeText] - False text manually added into address
  */
 export default function updateEmailLinkHrefs(
   scopeElement = document,
-  attributes
+  attributes = ATTRIBUTE_NAMES,
+  fakeText = FAKE_TEXT
 ) {
-  const selector = '[' + attributes.user + '][' + attributes.domain + ']';
-  const attributesMerged = Object.assign(attributes, ATTRIBUTE_NAMES);
+  const attrs = Object.assign(attributes, ATTRIBUTE_NAMES);
+  const selector = [
+    // For fake text, only search <a>, cuz <link> (irrelevant) supports `href`
+    'a[href*="' + fakeText + '"]',
+    // For attributes, search any element, so custom elements are supported
+    '[' + attrs.body + ']',
+    '[' + attrs.subject + ']',
+    '[' + attrs.user + '][' + attrs.domain + ']',
+  ].join(', ');
 
   scopeElement.querySelectorAll(selector).forEach(linkEl => {
-    const updateHref = _updateHref.bind(this, linkEl, attributesMerged);
+    const updateHref = _updateHref.bind(this, linkEl, attrs, fakeText);
 
     linkEl.addEventListener('click', updateHref, {once: true});
   });
