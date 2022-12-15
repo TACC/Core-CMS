2789f892c06ac9fe88e6bc3c31718fa1044b1279
John Gentle <taoteg@gmail.com>
2022-12-15 13:11:22 -0600

    Set the example settings_local to disable the portal nav by default so the example-cms runs correctly out-of-the-box.

--- taccsite_cms/settings_local.example.py
+++ taccsite_cms/settings_local.example.py
@@ -4,5 +4,5 @@ A `settings_local.py` file can override default values in `settings.py` and `set
 For a detailed walkthrough on overriding settings, see `settings_custom.example.py`.
 '''
 
-#INCLUDES_CORE_PORTAL = False
-#INCLUDES_PORTAL_NAV = False
+INCLUDES_CORE_PORTAL = False
+INCLUDES_PORTAL_NAV = False
