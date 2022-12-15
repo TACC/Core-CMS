8e2eec1c2e64d9c19ad35192f7fc96aef08b58e0
John Gentle <taoteg@gmail.com>
2022-12-15 14:45:42 -0600

    Aligned settings to defaults in core for comprehension ease.

--- taccsite_cms/settings_local.example.py
+++ taccsite_cms/settings_local.example.py
@@ -4,5 +4,9 @@ A `settings_local.py` file can override default values in `settings.py` and `set
 For a detailed walkthrough on overriding settings, see `settings_custom.example.py`.
 '''
 
+# Hide error about using Google Recaptcha test keys
+SILENCED_SYSTEM_CHECKS = ['captcha.recaptcha_test_key_error']
+
+# Disable the Core-Portal integration.
 INCLUDES_CORE_PORTAL = False
 INCLUDES_PORTAL_NAV = False
