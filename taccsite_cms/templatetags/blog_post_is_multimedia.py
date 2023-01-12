c902952b35153f57efd24193738cc389f311cdee
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-01-11 18:29:31 -0600

    feat: multimedia post custom media reads a setting
    - added setting TACC_BLOG_CUSTOM_MEDIA_POST_CATEGORY
    - changed template tag to read category_slug

--- taccsite_cms/templatetags/blog_post_is_multimedia.py
+++ taccsite_cms/templatetags/blog_post_is_multimedia.py
@@ -3,7 +3,7 @@ from django import template
 register = template.Library()
 
 @register.simple_tag
-def blog_post_is_multimedia(post=None):
+def blog_post_is_multimedia(post=None, category_slug=''):
     """
     Custom Template Tag `blog_post_is_multimedia`
 
@@ -14,18 +14,19 @@ def blog_post_is_multimedia(post=None):
 
     Template inline usage:
         {# (renders `True` or `False`) #}
-        {% blog_post_is_multimedia post %}
+        {% blog_post_is_multimedia post 'multimedia' %}
 
         {# (renders "A" or "B") #}
-        {% blog_post_is_multimedia post as is_multimedia %}
+        {% blog_post_is_multimedia post 'multimedia' as is_multimedia %}
         {% if is_multimedia %} A {% else %} B {% endif %}
 
     Example:
         ../templates/djangocms_blog/post_detail.html
     """
+    is_multimedia = False
     if post.categories.exists:
         for cat in post.categories.all():
-            if cat.slug == "multimedia":
+            if cat.slug == category_slug:
                 is_multimedia = True
 
     return is_multimedia
