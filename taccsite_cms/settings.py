c372d663639e60a346dc8f0f74b5fce99f84f902
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2023-02-03 16:40:54 -0600

    Task/auto responsive vdeo embed (#590)

--- taccsite_cms/settings.py
+++ taccsite_cms/settings.py
@@ -601,6 +601,7 @@ CKEDITOR_SETTINGS = {
 
 # https://github.com/django-cms/djangocms-video
 DJANGOCMS_VIDEO_TEMPLATES = [
+    ('responsive-auto', _('Responsive - Automatic')),
     ('responsive-16by9', _('Responsive - 16 by 9')),
     ('responsive-4by3', _('Responsive - 4 by 3')),
     ('responsive-1by1', _('Responsive - 1 by 1')),
