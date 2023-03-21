5a5eeef088257a7c76f4bbbeeb94753a144c2fbc
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2023-03-21 18:46:28 -0500

    feat(settings): wysiwyg customization (#595)
    * feat(settings): wysiwyg should not force <p> tags
    * fix(djangocms_text_ckeditor): styles opts & css

--- taccsite_cms/settings.py
+++ taccsite_cms/settings.py
@@ -585,7 +585,9 @@ META_USE_SCHEMAORG_PROPERTIES = True
 
 # https://github.com/django-cms/djangocms-text-ckeditor
 CKEDITOR_SETTINGS = {
-    'autoParagraph': False
+    'autoParagraph': False,
+    'stylesSet': 'default:/static/js/addons/ckeditor.wysiwyg.js',
+    'contentsCss': ['/static/djangocms_text_ckeditor/ckeditor/contents.css'],
 }
 
 # https://github.com/django-cms/djangocms-video
