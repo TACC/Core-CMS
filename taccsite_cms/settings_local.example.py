bee18dc6b754333ada4522987eb7f3c8e9d10d40
John Gentle <taoteg@gmail.com>
2023-03-24 13:11:32 -0500

    Task/update example cms defaults (#577)
    * Added secrets blob to gitignore for local dev convenience. Updated the example-cms assets and configs to all be self-contanied.
    
    * Added custom docker-compose files for local dev testing to ignore. May remove from ignore once vetted.
    
    * Added a make command to spin up the example cms. Still requires copying or symlinking to the example settings files.
    
    * Set the example settings_local to disable the portal nav by default so the example-cms runs correctly out-of-the-box.
    
    * Some tweaks to the settings_custom.example.py comments.
    
    * Adding back in the example files used by the mnake command for demoing the CMS.
    
    * Updated .gitignore but didnt add it.
    
    * Aligned settings to defaults in core for comprehension ease.
    
    * Aligned settings to defaults in core for comprehension ease.
    
    * chore: drop superfluous old secrets.py ignore rule
    
    ---------
    
    Co-authored-by: Wesley B <62723358+wesleyboar@users.noreply.github.com>
    Co-authored-by: Wesley Bomar <wbomar@tacc.utexas.edu>

--- taccsite_cms/settings_local.example.py
+++ taccsite_cms/settings_local.example.py
@@ -6,3 +6,7 @@ For a detailed walkthrough on overriding settings, see `settings_custom.example.
 
 # Hide error about using Google Recaptcha test keys
 SILENCED_SYSTEM_CHECKS = ['captcha.recaptcha_test_key_error']
+
+# Disable the Core-Portal integration.
+INCLUDES_CORE_PORTAL = False
+INCLUDES_PORTAL_NAV = False
