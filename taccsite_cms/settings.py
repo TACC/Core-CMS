82f7253644f9799184081c5352d4837f0f525636
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-04-05 14:04:56 -0500

    feat: add "OTHER' option in SearchEngine enum

--- taccsite_cms/settings.py
+++ taccsite_cms/settings.py
@@ -160,7 +160,7 @@ GOOGLE_ANALYTICS_PRELOAD = True
 
 # TODO: Consider adding 'GOOGLE' as an option
 # SEE: https://github.com/TACC/tup-ui/blob/6eb9412/apps/tup-cms/src/taccsite_cms/settings_custom.py#L101
-SearchEngines = Enum('SearchEngines', ['ELASTIC'])
+SearchEngines = Enum('SearchEngines', ['ELASTIC', 'OTHER'])
 
 SEARCH_ENGINE = SearchEngines.ELASTIC
 SEARCH_QUERY_PARAM_NAME = 'query_string'
