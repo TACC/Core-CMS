08815cfe7fe23af47554004b235aa3d120465946
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-06-12 18:46:22 -0500

    docs: comment improvments, typo fixes, removals

--- taccsite_cms/settings.py
+++ taccsite_cms/settings.py
@@ -549,7 +549,6 @@ SETTINGS_EXPORT_VARIABLE_NAME = 'settings'
 
 # https://github.com/django-cms/djangocms-style
 DJANGOCMS_STYLE_CHOICES = [
-    # https://tacc.utexas.edu/static/ui/components/detail/c-card--default.html
     'card',
     'card--plain',
     'card--standard',
@@ -557,7 +556,6 @@ DJANGOCMS_STYLE_CHOICES = [
     'card--image-bottom',
     'card--image-right',
     'card--image-left',
-    # https://tacc.utexas.edu/static/ui/components/detail/o-section.html
     'section',
     'section--light',
     'section--muted',
@@ -566,12 +564,9 @@ DJANGOCMS_STYLE_CHOICES = [
     'o-section o-section--style-light',
     'o-section o-section--style-muted',
     'o-section o-section--style-dark',
-    # https://cep.tacc.utexas.edu/design-system/ui-patterns/c-callout/
     'c-callout',
-    # https://cep.tacc.utexas.edu/design-system/ui-patterns/c-recognition/
     'c-recognition c-recognition--style-light',
     'c-recognition c-recognition--style-dark',
-    # https://ecepalliance.org/alliance-members/
     'c-nav', # bare-bones instance
     'c-nav c-nav--boxed',
 ]
