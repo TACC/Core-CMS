d0d934e674466deb2c61f0bcb382f16e064228b3
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-01-16 13:36:46 -0600

    docs(settings): TACC/djangocms-forms, not avryhof/
    We maintain our own fork now.

--- taccsite_cms/_settings/form_plugin.py
+++ taccsite_cms/_settings/form_plugin.py
@@ -12,7 +12,7 @@ from django.utils.translation import gettext_lazy as _
 
 ########################
 # DJANGOCMS_FORMS
-# https://github.com/avryhof/djangocms-forms/
+# https://github.com/TACC/djangocms-forms/
 ########################
 
 DJANGOCMS_FORMS_USE_HTML5_REQUIRED = True
