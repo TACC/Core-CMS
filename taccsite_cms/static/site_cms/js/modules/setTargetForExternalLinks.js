72e3d15847353f91b0e63751a36cc78bff7a8389
Tomas Gonzalez <tgonzalez@tacc.utexas.edu>
2024-02-29 12:34:18 -0600

    remove cruft for CMD-87

--- taccsite_cms/static/site_cms/js/modules/setTargetForExternalLinks.js
+++ taccsite_cms/static/site_cms/js/modules/setTargetForExternalLinks.js
@@ -1,37 +1,4 @@
-/**
- * Whether to log debug info to console
- * @const {string}
- */
-const SHOULD_DEBUG = window.DEBUG;
-
-/**
- * Function to perform after setting link target
- * @callback setTargetCallback
- * @param {HTMLElement} link
- */
-
-/**
- * Set external links (automatically discovered) to open in new tab
- * @param {object} [options] - Optional parameters
- * @param {array.<string>} [options.pathsToExernalSite=[]] - (DEPRECATED) A list of relative URL paths that should be treated like external URLs
- * @param {array.<string|RegExp>} [options.pathsToForceSetTarget=[]] - A list of relative URL paths (or patterns) that should trigger setting a target
- * @param {HTMLElement|Document} [options.scopeElement=document] - The element within which to search for links
- * @param {setTargetCallback} [options.setTargetCallback] - A callback for after a target is set
- */
-export default function findLinksAndSetTargets(options) {
-  const defaults = {
-    target: '_blank',
-    pathsToExernalSite: [],
-    pathsToForceSetTarget: [],
-    scopeElement: document
-  }
-  const {target, pathsToExernalSite, scopeElement, setTargetCallback} = {...defaults, ...options};
-  let {pathsToForceSetTarget} = {...defaults, ...options};
-
-  if ( pathsToExernalSite.length && ! pathsToForceSetTarget.length ) {
-    pathsToForceSetTarget = pathsToExernalSite;
-  }
-
+export default function findLinksAndSetTargets() {
   const links = scopeElement.getElementsByTagName('a');
   const baseDocumentHost = document.location.host;
   const baseDocumentHostWithSubdomain= `www.${baseDocumentHost}`;
