5ee05d35084172a90baf4a80e747fa01703f4d19
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-04-15 13:42:47 -0500

    style: remove extra new lines

--- taccsite_cms/templatetags/blog_cats_organized.py
+++ taccsite_cms/templatetags/blog_cats_organized.py
@@ -9,7 +9,7 @@ def filter_categories(categories):
 
     # Create a dictionary to store the indices of elements in the ordered list
     order_indices = {element: index for index, element in enumerate(TACC_BLOG_CATEGORY_ORDER)}
-    
+
     # Custom sorting function based on the indices in the ordered list
     def custom_sort(item):
         if item.slug in order_indices:
@@ -17,7 +17,7 @@ def filter_categories(categories):
         else:
             # For items not found in the ordered list, place them at the end
             return len(TACC_BLOG_CATEGORY_ORDER)
-    
+
     # Sort the categories list based on the custom sorting function
     sorted_categories = sorted(categories, key=custom_sort)
 
