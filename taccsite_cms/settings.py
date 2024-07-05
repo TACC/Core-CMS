552b7532f7689958d70fa88e33ed7b552d31294b
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-07-05 18:14:06 -0500

    Squashed merge of feat/CMD-165-custom-branding-via-setting:
    commit 85e6fa329f3feab7153d28ab05de96ad0bd5c857
    Author: Wesley Bomar <wbomar@tacc.utexas.edu>
    Date:   Fri Jul 5 18:13:13 2024 -0500
    
        fix: CMD-165 allow settings_local BRANDING
    
    commit 9a73ba572626cb6feaddf8c5eb66976eb32639b8
    Author: Wesley Bomar <wbomar@tacc.utexas.edu>
    Date:   Fri Jul 5 16:42:28 2024 -0500
    
        fix: CMD-165 BRANDING loop values accessed wrong
    
    commit 265bab337030e591757e57eb9528fdb18f659e8b
    Author: Wesley Bomar <wbomar@tacc.utexas.edu>
    Date:   Fri Jul 5 16:06:18 2024 -0500
    
        chore: CMD-165 remove BRANDING from primary export
    
    commit af10ad4b8fbc7da6c9c6e6bae05b3f359498bbbd
    Merge: ff6405d1 deee0e44
    Author: Wesley Bomar <wbomar@tacc.utexas.edu>
    Date:   Fri Jul 5 16:02:33 2024 -0500
    
        Merge branch 'main' into feat/CMD-165-custom-branding-via-setting
    
    commit ff6405d1801714508ee2193328165d927231a8c9
    Author: Wesley Bomar <wbomar@tacc.utexas.edu>
    Date:   Fri Jul 5 15:47:33 2024 -0500
    
        fix: CMD-165 support deprecated BRANDING setting
    
    commit f56a0dde7bffb0c5a3bdd0de91ff537a384b3cc6
    Author: Wesley Bomar <wbomar@tacc.utexas.edu>
    Date:   Fri Jul 5 15:40:32 2024 -0500
    
        feat: CMD-165 render new or old custom branding
    
    commit 4c6ee29b3f3b921f17fc108ef8a1112c106c8349
    Author: Wesley Bomar <wbomar@tacc.utexas.edu>
    Date:   Fri Jul 5 15:00:50 2024 -0500
    
        feat: CMD-165 custom branding via setting

--- taccsite_cms/settings.py
+++ taccsite_cms/settings.py
@@ -220,33 +220,7 @@ GOOGLE_ANALYTICS_PRELOAD = True
 # TACC: BRANDING
 ########################
 
-PORTAL_BRANDING_TACC = {
-    "is_remote": False,
-    "img_file_src": "site_cms/img/org_logos/tacc-white.png",
-    "img_class": "branding-tacc",
-    "link_href": "https://www.tacc.utexas.edu/",
-    "link_target": "_blank",
-    "img_alt_text": "TACC Logo",
-    "img_crossorigin": "anonymous",
-}
-PORTAL_BRANDING_UTEXAS = {
-    "is_remote": False,
-    "img_file_src": "site_cms/img/org_logos/utaustin-white.png",
-    "img_class": "branding-utaustin",
-    "link_href": "https://www.utexas.edu/",
-    "link_target": "_blank",
-    "img_alt_text": "University of Texas at Austin Logo",
-    "img_crossorigin": "anonymous",
-}
-PORTAL_BRANDING_NSF = {
-    "is_remote": False,
-    "img_file_src": "site_cms/img/org_logos/nsf-white.png",
-    "img_class": "branding-nsf",
-    "link_href": "https://www.nsf.gov/",
-    "link_target": "_blank",
-    "img_alt_text": "NSF Logo",
-    "img_crossorigin": "anonymous",
-}
+from taccsite_cms._settings.branding import *
 
 # To hide branding, set `PORTAL_BRANDING = False`
 PORTAL_BRANDING = [ PORTAL_BRANDING_TACC, PORTAL_BRANDING_UTEXAS ]
