646b26db8b2d377286323a11f8b9709933640870
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-07-22 15:20:20 -0500

    fix: TUP-706 render feed ⚠️
    ⚠️ Does NOT render feed items.

--- apps/djangocms_blog_customizations/feeds.py
+++ apps/djangocms_blog_customizations/feeds.py
@@ -1,17 +1,32 @@
-from html import unescape
-from io import BytesIO
-
 from aldryn_apphooks_config.utils import get_app_instance
-from django.contrib.sites.models import Site
-from django.urls import reverse
 from django.utils.feedgenerator import Rss201rev2Feed
-from django.utils.translation import gettext as _
 
 from djangocms_blog.feeds import LatestEntriesFeed as DjangoCMSBlogLatestEntriesFeed
 from djangocms_blog.settings import get_setting
-from djangocms_blog.models import Post
+from djangocms_blog.cms_appconfig import BlogConfig
 
+import logging
+
+logger = logging.getLogger(f"portal.{__name__}")
 
 class LatestEntriesFeed(DjangoCMSBlogLatestEntriesFeed):
     feed_type = Rss201rev2Feed
     feed_items_number = get_setting("FEED_LATEST_ITEMS")
+
+    link = "/blog/"
+
+    def __call__(self, request, *args, **kwargs):
+        namespace = get_setting("AUTO_NAMESPACE")
+
+        self.request = request
+        self.namespace = get_setting("AUTO_NAMESPACE")
+        self.config = BlogConfig.objects.get(namespace=namespace)
+
+        logger.debug(f"CUSTOM FEED | NAMESPACE: {self.namespace}, CONFIG: {self.config}")
+
+        return super().__call__(request, *args, **kwargs)
+
+    def items(self):
+        items = super().items()
+        logging.debug(f"CUSTOM FEED | FEED ITEMS: {items}")
+        return items
--- apps/djangocms_blog_customizations/urls.py
+++ apps/djangocms_blog_customizations/urls.py
@@ -1,12 +1,13 @@
 from django.urls import re_path
+
 from .feeds import LatestEntriesFeed
 from .views import BlogView, BlogRemoteView
 
 
 app_name = 'djangocms_blog_customizations'
 urlpatterns = [
-    # Fails with `NoReverseMatch`
-    # path('feed', LatestEntriesFeed(), name="djangocms_blog_customizations_feed"),
+    # WARNING: Renders feed, but does nto render items within the feed
+    re_path(r'^feed/', LatestEntriesFeed(), name='djangocms_blog_customizations_feed'),
 
     re_path(r'^local/', BlogView, name='djangocms_blog_customizations_base'),
     re_path(r'^remote/', BlogRemoteView, name='djangocms_blog_customizations_remote'),
