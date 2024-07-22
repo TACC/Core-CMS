aa709fddb6074ce1e878495fb3ffe1325de6bd9b
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-07-22 15:29:09 -0500

    docs: TUP-706 describe each URL's problem

--- apps/djangocms_blog_customizations/urls.py
+++ apps/djangocms_blog_customizations/urls.py
@@ -6,9 +6,12 @@ from .views import BlogView, BlogRemoteView
 
 app_name = 'djangocms_blog_customizations'
 urlpatterns = [
-    # WARNING: Renders feed, but does nto render items within the feed
-    re_path(r'^feed/', LatestEntriesFeed(), name='djangocms_blog_customizations_feed'),
+    # XXX: Does NOT render items within the feed
+    re_path(r'^feed/', LatestEntriesFeed(), name='feed'),
 
-    re_path(r'^local/', BlogView, name='djangocms_blog_customizations_base'),
-    re_path(r'^remote/', BlogRemoteView, name='djangocms_blog_customizations_remote'),
+    # XXX: Does not render blog content
+    re_path(r'^local/', BlogView, name='base'),
+
+    # XXX: URLs are still local
+    re_path(r'^remote/', BlogRemoteView, name='remote'),
 ]
