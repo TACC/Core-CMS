db234df5aa7cc9d0e892d3c7c45231fb218db83f
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-07-22 12:10:12 -0500

    feat!: TUP-706 render remote url content
    Load `/blog/remote`.
    
    BREAKING CHANGE: Changed `/blog` to `/blog/local`

--- apps/djangocms_blog_customizations/urls.py
+++ apps/djangocms_blog_customizations/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .feeds import LatestEntriesFeed
-from .views import BlogView
+from .views import BlogView, BlogRemoteView
 
 
 app_name = 'djangocms_blog_customizations'
@@ -8,5 +8,6 @@ urlpatterns = [
     # Fails with `NoReverseMatch`
     # path('feed', LatestEntriesFeed(), name="djangocms_blog_customizations_feed"),
 
-    path('', BlogView, name='djangocms_blog_customizations_base'),
+    path('local', BlogView, name='djangocms_blog_customizations_base'),
+    path('remote', BlogRemoteView, name='djangocms_blog_customizations_remote'),
 ]
--- apps/djangocms_blog_customizations/views.py
+++ apps/djangocms_blog_customizations/views.py
@@ -1,5 +1,14 @@
+from django.http import HttpResponse
+from django.template import Template, Context
 from django.shortcuts import render
+import requests
 
 
 def BlogView(request):
     return render(request, 'djangocms_blog/base.html')
+
+def BlogRemoteView(request):
+    request = requests.get('https://www.example.com/')
+    request.text
+
+    return HttpResponse(Template(request.text).render(Context({})))
