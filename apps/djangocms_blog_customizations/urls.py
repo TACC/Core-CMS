26a9412949f4d519a1167f495fd99fc7a5ee2497
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-07-22 15:36:10 -0500

    docs: TUP-706 describe each URL goal and status

--- apps/djangocms_blog_customizations/urls.py
+++ apps/djangocms_blog_customizations/urls.py
@@ -6,12 +6,18 @@ from .views import BlogView, BlogRemoteView
 
 app_name = 'djangocms_blog_customizations'
 urlpatterns = [
+    # To render styled Blog feed
+    # XXX: Does NOT style the feed
     # XXX: Does NOT render items within the feed
     re_path(r'^feed/', LatestEntriesFeed(), name='feed'),
 
-    # XXX: Does not render blog content
+    # To render current blog, content only (no Core-CMS base.html)
+    # XXX: Does NOT render blog content
+    # XXX: Does NOT escape Core-CMS base.html
     re_path(r'^local/', BlogView, name='base'),
 
+    # To render a blog (or any page) from another website
     # XXX: URLs are still local
+    # XXX: Is NOT blog-specific, so should be a generic feature
     re_path(r'^remote/', BlogRemoteView, name='remote'),
 ]
