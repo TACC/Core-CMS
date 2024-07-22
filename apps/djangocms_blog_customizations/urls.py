8038de703bbbce7e31511c6975e212e34f13c314
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-07-22 11:54:46 -0500

    fix: TUP-706 disable failed djangocms_blog_c… url

--- apps/djangocms_blog_customizations/urls.py
+++ apps/djangocms_blog_customizations/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .feeds import LatestEntriesFeed
 
+
 app_name = 'djangocms_blog_customizations'
 urlpatterns = [
-    path('', LatestEntriesFeed(), name="custom-posts-latest-feed"),
+    # Fails with `NoReverseMatch`
+    # path('feed', LatestEntriesFeed(), name="djangocms_blog_customizations_feed"),
 ]
