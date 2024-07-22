ec10c853ce5f7f3a010014d7bf7231055a415859
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-07-22 12:35:09 -0500

    feat: TUP-706 render remote url via setting

--- apps/djangocms_blog_customizations/views.py
+++ apps/djangocms_blog_customizations/views.py
@@ -1,5 +1,6 @@
 import requests
 
+from django.conf import settings
 from django.http import HttpResponse
 from django.template import Template, Context
 from django.shortcuts import render
@@ -8,7 +9,7 @@ def BlogView(request):
     return render(request, 'djangocms_blog/base.html')
 
 def BlogRemoteView(request):
-    request = requests.get('https://www.example.com/')
+    request = requests.get(settings.PORTAL_BLOG_REMOTE_URL)
     request.text
 
     return HttpResponse(Template(request.text).render(Context({})))
