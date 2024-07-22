d7f10ae32b7936117725660af7398cec8f192c56
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-07-22 12:23:02 -0500

    chore: TUP-706 move an import higher in file

--- apps/djangocms_blog_customizations/views.py
+++ apps/djangocms_blog_customizations/views.py
@@ -1,8 +1,8 @@
+import requests
+
 from django.http import HttpResponse
 from django.template import Template, Context
 from django.shortcuts import render
-import requests
-
 
 def BlogView(request):
     return render(request, 'djangocms_blog/base.html')
