0c9440160ef381a8dcfc82ebfd81bf14180f3693
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2024-09-11 13:28:09 -0500

    feat: TUP-706 edit links via get_client_markup()

--- apps/djangocms_blog_customizations/views.py
+++ apps/djangocms_blog_customizations/views.py
@@ -13,29 +13,35 @@ class BlogRemoteView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        remote_url = self.get_remote_url()
-        remote_content = self.get_remote_content(remote_url)
+        source_url = self.get_source_url()
+        source_markup = self.get_source_markup(source_url)
+        client_markup = self.get_client_markup(source_markup)
 
-        context['markup'] = remote_content
+        context['markup'] = client_markup
 
         return context
 
-    def get_remote_url(self):
-        path = self.request.path.replace(
-            settings.PORTAL_BLOG_REMOTE_CLIENT_PATH,
-            ''
-        )
-        url = (
-            settings.PORTAL_BLOG_REMOTE_SOURCE_ROOT
-            + path
-        )
+    def get_source_url(self):
+        client_path = settings.PORTAL_BLOG_REMOTE_CLIENT_PATH
+        source_host = settings.PORTAL_BLOG_REMOTE_SOURCE_ROOT
+        source_path = self.request.path.replace(client_path, '')
 
-        return url
+        return source_host + source_path
 
-    def get_remote_content(self, url):
+    def get_source_markup(self, url):
         response = requests.get(url)
 
         if response.status_code == 200:
             return response.text
+        else:
+            return None
+
+    # CAVEAT: Causes request to PORTAL_BLOG_REMOTE_CLIENT_PATH for every 'href'
+    def get_client_markup(self, source_markup):
+        client_path_prefix = settings.PORTAL_BLOG_REMOTE_CLIENT_PATH
+
+        # FAQ: No markup for bad URL or a resource specific to source wesbite
+        if source_markup:
+            return source_markup.replace('href="', 'href="' + client_path_prefix)
         else:
             return None
