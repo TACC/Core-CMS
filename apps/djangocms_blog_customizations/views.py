49edd04b8884375e8830aacf444cc13705e9c18d
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2024-09-11 19:38:57 -0500

    fix: TUP-706 broken links
    - ✓ fix links to articles
    - ✓ fix pagination links

--- apps/djangocms_blog_customizations/views.py
+++ apps/djangocms_blog_customizations/views.py
@@ -1,5 +1,7 @@
 import requests
 
+from urllib.parse import urlparse
+
 from django.conf import settings
 from django.template import Template, Context
 from django.shortcuts import render
@@ -23,10 +25,18 @@ class BlogRemoteView(TemplateView):
 
     def get_source_url(self):
         client_path = settings.PORTAL_BLOG_REMOTE_CLIENT_PATH
-        source_host = settings.PORTAL_BLOG_REMOTE_SOURCE_ROOT
-        source_path = self.request.path.replace(client_path, '')
+        source_root = settings.PORTAL_BLOG_REMOTE_SOURCE_ROOT
+        client = urlparse(self.request.build_absolute_uri())
+        source = urlparse(source_root)
 
-        return source_host + source_path
+        source_path = source.path + client.path.replace(client_path, '')
+        source_url = client._replace(
+            scheme=source.scheme,
+            netloc=source.netloc,
+            path=source_path,
+        ).geturl()
+
+        return source_url
 
     def get_source_markup(self, url):
         response = requests.get(url)
@@ -36,12 +46,16 @@ class BlogRemoteView(TemplateView):
         else:
             return None
 
-    # CAVEAT: Causes request to PORTAL_BLOG_REMOTE_CLIENT_PATH for every 'href'
+    # CAVEAT: Causes a view request for every resource (img/script/stylesheet)
     def get_client_markup(self, source_markup):
-        client_path_prefix = settings.PORTAL_BLOG_REMOTE_CLIENT_PATH
+        source = urlparse(settings.PORTAL_BLOG_REMOTE_SOURCE_ROOT)
+        client_path = settings.PORTAL_BLOG_REMOTE_CLIENT_PATH
 
         # FAQ: No markup for bad URL or a resource specific to source wesbite
         if source_markup:
-            return source_markup.replace('href="', 'href="' + client_path_prefix)
+            return source_markup.replace(
+                'href="' + source.path,
+                'href="' + client_path
+            )
         else:
             return None
