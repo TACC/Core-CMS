b4444f08f6776740766d8aaf10cb85695ada6e47
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2024-10-22 16:50:02 -0500

    fix: blog install steps are outdated

--- taccsite_cms/custom_app_settings.example.py
+++ taccsite_cms/custom_app_settings.example.py
@@ -1,3 +1,15 @@
-CUSTOM_APPS = ['apps.custom_example']
+CUSTOM_APPS = [
+
+    # CUSTOM APP
+    'apps.custom_example'
+
+    # DJANGOCMS_BLOG
+    'parler',
+    'taggit',
+    'taggit_autosuggest',
+    'sortedm2m',
+    'djangocms_blog',
+
+]
 CUSTOM_MIDDLEWARE = []
 STATICFILES_DIRS = ()
--- taccsite_cms/settings_custom.example.py
+++ taccsite_cms/settings_custom.example.py
@@ -114,49 +114,11 @@ PORTAL_SEARCH_INDEX_IS_AUTOMATIC = False
 # DJANGOCMS_BLOG
 ########################
 
-from taccsite_cms.settings import INSTALLED_APPS
-
-tacc_app_index = INSTALLED_APPS.index('taccsite_cms')
-INSTALLED_APPS[tacc_app_index:tacc_app_index] = [
-    # 'aldryn_apphooks_config' # already in Core
-    # 'filer',              # already in Core
-    # 'easy_thumbnails',    # already in Core
-    'parler',
-    'taggit',
-    'taggit_autosuggest',
-    # 'meta',               # already in Core
-    'sortedm2m',
-    'djangocms_blog',
-]
-# REQ: 'taggit_autosuggest' requires the following is added to `urls.py`
-# FAQ: For local Core-CMS or any Core-CMS-Custom app, add to `urls_custom.py`
-"""
-from django.urls import re_path, include
-
-urlpatterns += [
-    # Support `taggit_autosuggest` (from `djangocms-blog`)
-    re_path(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
-]
-"""
-
-# Paths for alternate templates that user can choose for blog-specific plugin
-# - Devs can customize core templates at `templates/djangocms_blog/`.
-# - Users can choose alt. templates from `templates/djangocms_blog/plugins/*`.
-# - Devs can customize alt. templates at `templates/djangocms_blog/plugins/*`.
-BLOG_PLUGIN_TEMPLATE_FOLDERS = (
-    ('plugins', 'Default'),
-    # ('plugins/alternate', 'Alternate'),
-)
-
-# Change default values for the auto-setup of one `BlogConfig`
-# SEE: https://github.com/nephila/djangocms-blog/issues/629
 BLOG_AUTO_SETUP = True # Set to False after setup (minimize overhead)
 BLOG_AUTO_HOME_TITLE ='Home'
 BLOG_AUTO_BLOG_TITLE = 'News'
 BLOG_AUTO_APP_TITLE = 'News'
 BLOG_AUTO_NAMESPACE = 'News'
-
-# Miscellaneous settings
 BLOG_ENABLE_COMMENTS = False
 
 ########################
@@ -165,10 +127,3 @@ BLOG_ENABLE_COMMENTS = False
 
 PORTAL_BLOG_SHOW_CATEGORIES = True
 PORTAL_BLOG_SHOW_TAGS = True
-
-########################
-# DJANGOCMS_BLOG: DJANGO
-########################
-
-# TACC/Core-CMS-Resources#75: Load custom urls.py so we can add urlpatterns for taggit_autosuggest
-ROOT_URLCONF = 'taccsite_custom.example_cms.urls'
