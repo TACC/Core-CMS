a9fe6ec03927b6419286e4c6f1624ff4d621b837
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2024-10-22 16:59:58 -0500

    fix: blog install steps are outdated (#884)

--- taccsite_cms/urls_custom.example.py
+++ taccsite_cms/urls_custom.example.py
@@ -1,5 +1,11 @@
 from django.urls import re_path, include
 
 custom_urls = [
+
+    # CUSTOM APP
     re_path(r'^custom_test/', include('apps.custom_example.urls', namespace='custom_test')),
+
+    # DJANGOCMS_BLOG
+    re_path(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
+
 ]
