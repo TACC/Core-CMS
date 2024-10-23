555d988c60f4a1bc6fdd61b3c254faf0ea3192a6
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2024-10-22 19:12:34 -0500

    docs: remove excess whtiespace

--- taccsite_cms/settings_custom.example.py
+++ taccsite_cms/settings_custom.example.py
@@ -33,8 +33,7 @@ CMS_TEMPLATES = (
     ('standard.html', 'Standard'),
     ('fullwidth.html', 'Full Width'),
 
-    # WARNING: Not intuitive to unset, so only enable as needed e.g.
-    #          - serve Blog such that it can be injected into another CMS
+    # WARNING: Not intuitive to unset, so only enable as needed e.g. serve Blog such that it can be injected into another CMS
     # ('raw.html', 'Raw'),
 )
 
