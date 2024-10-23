3291015f8578056a843ef6dcea220e5b2f8be43d
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2024-10-23 18:21:46 -0500

    enhance(search): attach apphook to manage urls

--- apps/search_page/utils.py
+++ apps/search_page/utils.py
@@ -5,27 +5,48 @@ from django.conf import settings
 from cms.api import create_page
 from cms.models.pagemodel import Page
 
+from .cms_apps import SearchPageApphook
+
 logger = logging.getLogger(f'portal.{__name__}')
 
 TITLE = 'Search'
-SLUG = settings.PORTAL_SEARCH_PATH.strip('/')
+DEFAULT_SLUG = settings.PORTAL_SEARCH_PATH.strip('/')
+
+def get_page():
+    try:
+        page = Page.objects.filter(reverse_id='search_page').first()
+        return page
+    except Page.DoesNotExist:
+        return None
+
+def get_slug(page=None):
+    if page:
+        return page.get_slug()
+    else:
+        page = get_page()
+        return get_slug() if page else DEFAULT_SLUG
 
 def create_search_page():
-    has_search_page = Page.objects.filter(title_set__slug=SLUG).exists()
+    page = get_page()
+    slug = get_slug(page)
 
-    if not has_search_page:
-        create_page(
+    if not page:
+        page = create_page(
             title=f'{TITLE} (Auto-Generated)',
             menu_title=TITLE,
             page_title=TITLE,
-            reverse_id=SLUG,
+            reverse_id='search_page',
             # Use a template from CMS_TEMPLATES setting
             template='standard.html',
             language='en',
             published=True,
-            slug=SLUG,
+            slug=slug,
             in_navigation=False,
+            apphook=SearchPageApphook,
+            apphook_namespace=SearchPageApphook.name,
         )
-        logger.info(f'Created search page "{TITLE}" at "{SLUG}"')
+        logger.info(f'Created search page "{TITLE}" at "{slug}"')
     else:
-        logger.info(f'Found existing search page at "{SLUG}"')
+        logger.info(f'Found existing search page at "{slug}"')
+
+    return page
--- taccsite_cms/urls_custom.example.py
+++ taccsite_cms/urls_custom.example.py
@@ -6,9 +6,6 @@ custom_urls = [
     # CUSTOM APP
     re_path(r'^custom_test/', include('apps.custom_example.urls', namespace='custom_test')),
 
-    # SEARCH_PAGE
-    re_path(r'^' + settings.PORTAL_SEARCH_PATH.strip('/') + '/', include('apps.search_page.urls', namespace='search')),
-
     # DJANGOCMS_BLOG
     re_path(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
 
