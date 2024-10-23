ad24ead570e10c6c8a4e5245843e2bb2be8294f2
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2024-10-23 16:12:07 -0500

    feat(search): create page if it does not exist

--- apps/search_page/apps.py
+++ apps/search_page/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class SearchPageConfig(AppConfig):
     name = 'apps.search_page'
+
+    def ready(self):
+        from .utils import create_search_page
+        create_search_page()
