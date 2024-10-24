8fd1828477719c2d2b031851c9e396273e99e831
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2024-10-23 19:45:38 -0500

    chore(search): reduce silly complexity of tag

--- taccsite_cms/templatetags/search_tags.py
+++ taccsite_cms/templatetags/search_tags.py
@@ -6,14 +6,9 @@ register = template.Library()
 
 @register.simple_tag
 def search_page_url():
-    try:
-        page = Page.objects.filter(reverse_id='search_page').first()
-        if page:
-            return page.get_absolute_url()
-        else:
-            return settings.PORTAL_SEARCH_PATH
-    except Exception as e:
-        # Log the error if needed
-        print('Error with tag "search_page_url":', e)
-        # To fallback to a default URL if the apphook isn't active
+    page = Page.objects.filter(reverse_id='search_page').first()
+
+    if page:
+        return page.get_absolute_url()
+    else:
         return settings.PORTAL_SEARCH_PATH
