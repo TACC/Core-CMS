b1bfb23f90be023e534023febbea791a954d4a69
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2024-11-11 18:51:05 -0600

    feat: grid editor (⚠️ untested)

---
+++ taccsite_cms/management/commands/group_perms/grid_editor_advanced.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import Group
+
+from .util import add_perm
+
+def set_group_perms():
+    group, was_created = Group.objects.get_or_create(
+        name='Grid Editor (Advanced)'
+    )
+
+    add_perm(group, 'cms', 'page', 'Can change page')
+    add_perm(group, 'cms', 'page', 'Can view page')
+
+    add_perm(group, 'cms', 'placeholder', 'Can use Structure mode')
+
+    add_perm(group, 'bootstrap4_grid', 'bootstrap4 grid container', 'Can add bootstrap4 grid container')
+    add_perm(group, 'bootstrap4_grid', 'bootstrap4 grid container', 'Can change bootstrap4 grid container')
+    add_perm(group, 'bootstrap4_grid', 'bootstrap4 grid container', 'Can delete bootstrap4 grid container')
+    add_perm(group, 'bootstrap4_grid', 'bootstrap4 grid container', 'Can view bootstrap4 grid container')
+
+    add_perm(group, 'bootstrap4_grid', 'bootstrap4 grid column', 'Can add bootstrap4 column')
+    add_perm(group, 'bootstrap4_grid', 'bootstrap4 grid column', 'Can change bootstrap4 column')
+    add_perm(group, 'bootstrap4_grid', 'bootstrap4 grid column', 'Can delete bootstrap4 column')
+    add_perm(group, 'bootstrap4_grid', 'bootstrap4 grid column', 'Can view bootstrap4 column')
+
+    add_perm(group, 'bootstrap4_grid', 'bootstrap4 grid rw', 'Can add bootstrap4 rw')
+    add_perm(group, 'bootstrap4_grid', 'bootstrap4 grid rw', 'Can change bootstrap4 rw')
+    add_perm(group, 'bootstrap4_grid', 'bootstrap4 grid rw', 'Can delete bootstrap4 rw')
+    add_perm(group, 'bootstrap4_grid', 'bootstrap4 grid rw', 'Can view bootstrap4 rw')
