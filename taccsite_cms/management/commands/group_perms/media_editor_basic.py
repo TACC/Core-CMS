a88e9f8f0f51bef0b55e8407def47d9501d61900
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2024-11-18 16:11:38 -0600

    refactor: less calls to `add_perm` (#899)
    * refactor: consolidate add_perm calls (⚠️ untested)
    
    * refactor: news_writer (⚠️ untested) & renames
    
    * fix: user lacks [Edit] / [View published] buttons
    
    * fix: errors adding permissions

--- taccsite_cms/management/commands/group_perms/media_editor_basic.py
+++ taccsite_cms/management/commands/group_perms/media_editor_basic.py
@@ -1,36 +1,22 @@
 from django.contrib.auth.models import Group
 
-from .util import add_perm
+from .util import (
+    let_view_and_change_page_structure,
+    let_view_and_change_media_plugins,
+    let_view_and_change_adv_media_plugins,
+    let_view_thumbnail_option,
+    let_view_folder,
+    let_view_and_change_image_file,
+)
 
 def set_group_perms():
     group, was_created = Group.objects.get_or_create(
         name='Media Editor (Basic)'
     )
 
-    add_perm(group, 'cms', 'page', 'Can change page')
-
-    add_perm(group, 'cms', 'placeholder', 'Can use Structure mode')
-
-    add_perm(group, 'bootstrap4_picture', 'bootstrap4 picture', 'Can change bootstrap4 picture')
-    add_perm(group, 'bootstrap4_picture', 'bootstrap4 picture', 'Can view bootstrap4 picture')
-
-    add_perm(group, 'djangocms_picture', 'picture', 'Can change picture')
-    add_perm(group, 'djangocms_picture', 'picture', 'Can view picture')
-
-    add_perm(group, 'djangocms_video', 'video player', 'Can change video player')
-    add_perm(group, 'djangocms_video', 'video player', 'Can view video player')
-    add_perm(group, 'djangocms_video', 'video source', 'Can change video source')
-    add_perm(group, 'djangocms_video', 'video source', 'Can view video source')
-    add_perm(group, 'djangocms_video', 'video track', 'Can change video track')
-    add_perm(group, 'djangocms_video', 'video track', 'Can view video track')
-
-    add_perm(group, 'filer', 'file', 'Can change file')
-    add_perm(group, 'filer', 'file', 'Can view file')
-
-    add_perm(group, 'filer', 'Folder', 'Can use directory listing')
-    add_perm(group, 'filer', 'Folder', 'Can view Folder')
-
-    add_perm(group, 'filer', 'image', 'Can change image')
-    add_perm(group, 'filer', 'image', 'Can view image')
-
-    add_perm(group, 'filer', 'thumbnail option', 'Can view thumbnail option')
+    let_view_and_change_page_structure(group)
+    let_view_and_change_media_plugins(group)
+    let_view_and_change_adv_media_plugins(group)
+    let_view_folder(group)
+    let_view_and_change_image_file(group)
+    let_view_thumbnail_option(group)
--- taccsite_cms/management/commands/group_perms/news_writer_advanced.py
+++ taccsite_cms/management/commands/group_perms/news_writer_advanced.py
@@ -1,49 +1,50 @@
 from django.contrib.auth.models import Group
 
-from .util import add_perm
+from .util import (
+    add_perm,
+    let_view_and_change_page_structure,
+    let_view_and_change_plugin,
+    let_view_and_change_text,
+    let_add_and_delete_text,
+    let_view_and_change_media_plugins,
+    let_add_and_delete_media_plugins,
+    let_view_and_change_image_file,
+)
 
 def set_group_perms():
     group, was_created = Group.objects.get_or_create(
         name='News Writer (Advanced)'
     )
 
-    add_perm(group, 'bootstrap4_blockquote', 'bootstrap4 blockquote', 'Can add bootstrap4 blockquote')
-    add_perm(group, 'bootstrap4_blockquote', 'bootstrap4 blockquote', 'Can change bootstrap4 blockquote')
-    add_perm(group, 'bootstrap4_blockquote', 'bootstrap4 blockquote', 'Can delete bootstrap4 blockquote')
-    add_perm(group, 'bootstrap4_blockquote', 'bootstrap4 blockquote', 'Can view bootstrap4 blockquote')
-    add_perm(group, 'bootstrap4_picture', 'bootstrap4 picture', 'Can add bootstrap4 picture')
-    add_perm(group, 'bootstrap4_picture', 'bootstrap4 picture', 'Can change bootstrap4 picture')
-    add_perm(group, 'bootstrap4_picture', 'bootstrap4 picture', 'Can delete bootstrap4 picture')
-    add_perm(group, 'bootstrap4_picture', 'bootstrap4 picture', 'Can view bootstrap4 picture')
-    add_perm(group, 'cms', 'cms plugin', 'Can change cms plugin')
-    add_perm(group, 'cms', 'cms plugin', 'Can view cms plugin')
-    add_perm(group, 'cms', 'cms plugin', 'Can add cms plugin')
-    add_perm(group, 'cms', 'cms plugin', 'Can delete cms plugin')
-    add_perm(group, 'cms', 'page', 'Can change page')
-    add_perm(group, 'cms', 'page', 'Can view page')
-    add_perm(group, 'cms', 'placeholder', 'Can use Structure mode')
-    add_perm(group, 'djangocms_blog', 'blog category', 'Can add blog category')
-    add_perm(group, 'djangocms_blog', 'blog category', 'Can change blog category')
-    add_perm(group, 'djangocms_blog', 'blog category', 'Can delete blog category')
-    add_perm(group, 'djangocms_blog', 'blog category', 'Can view blog category')
-    add_perm(group, 'djangocms_blog', 'blog article', 'Can add blog article')
-    add_perm(group, 'djangocms_blog', 'blog article', 'Can change blog article')
-    add_perm(group, 'djangocms_blog', 'blog article', 'Can view blog article')
-    add_perm(group, 'djangocms_link', 'link', 'Can add link')
-    add_perm(group, 'djangocms_link', 'link', 'Can change link')
-    add_perm(group, 'djangocms_link', 'link', 'Can delete link')
-    add_perm(group, 'djangocms_link', 'link', 'Can view link')
-    add_perm(group, 'djangocms_text_ckeditor', 'text', 'Can add text')
-    add_perm(group, 'djangocms_text_ckeditor', 'text', 'Can change text')
-    add_perm(group, 'djangocms_text_ckeditor', 'text', 'Can delete text')
-    add_perm(group, 'djangocms_text_ckeditor', 'text', 'Can view text')
-    add_perm(group, 'djangocms_video', 'video player', 'Can add video player')
-    add_perm(group, 'djangocms_video', 'video player', 'Can change video player')
-    add_perm(group, 'djangocms_video', 'video player', 'Can delete video player')
-    add_perm(group, 'djangocms_video', 'video player', 'Can view video player')
+    let_view_and_change_page_structure(group)
+    let_view_and_change_plugin(group)
+
+    let_view_and_change_text(group)
+    let_add_and_delete_text(group)
+    # NOTE: Consider adding these to `let…text` functions
+    add_perm(group, 'bootstrap4_content', 'bootstrap4 blockquote', 'Can add bootstrap4 blockquote')
+    add_perm(group, 'bootstrap4_content', 'bootstrap4 blockquote', 'Can change bootstrap4 blockquote')
+    add_perm(group, 'bootstrap4_content', 'bootstrap4 blockquote', 'Can delete bootstrap4 blockquote')
+    add_perm(group, 'bootstrap4_content', 'bootstrap4 blockquote', 'Can view bootstrap4 blockquote')
+
+    let_view_and_change_media_plugins(group)
+    let_add_and_delete_media_plugins(group)
+
+    let_view_and_change_image_file(group)
     add_perm(group, 'filer', 'image', 'Can add image')
-    add_perm(group, 'filer', 'image', 'Can change image')
-    add_perm(group, 'filer', 'image', 'Can view image')
+
+    # Add permissions to view & change & add articles
+    add_perm(group, None, None, 'Can view blog article')
+    add_perm(group, None, None, 'Can change blog article')
+    add_perm(group, None, None, 'Can add blog article')
+
+    # Add permissions to manage categories
+    add_perm(group, None, None, 'Can add blog category')
+    add_perm(group, None, None, 'Can change blog category')
+    add_perm(group, None, None, 'Can delete blog category')
+    add_perm(group, None, None, 'Can view blog category')
+
+    # Add permissions to manage tags & tagged items (e.g. articles)
     add_perm(group, 'taggit', 'tag', 'Can add tag')
     add_perm(group, 'taggit', 'tag', 'Can change tag')
     add_perm(group, 'taggit', 'tag', 'Can delete tag')
