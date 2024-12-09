dd2f4c6718da824ee2182221c0a2ec5773a033e1
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2024-12-09 17:26:47 -0600

    fix: do not commit default custom settings

--- taccsite_cms/settings_default.py
+++
@@ -1,5 +0,0 @@
-'''
-A `settings_default.py` file can override default values in `settings.py` before `settings_custom.py`.
-
-This allows Core-CMS client software to standardize their custom settings while still supporting `settings_custom.py` e.g. https://github.com/TACC/Core-Portal/pull/1034.
-'''
