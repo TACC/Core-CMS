6c4d1a83efe84b1c7d1b6e1de74d7ed1ef40c98f
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-02-04 18:30:25 -0600

    feat: better group perms mgmt (#895)
    * refactor: simpler syntax for group perms (#894)
    
    * chore: remove unused imports
    
    * refactor: add & use add_perm utility function
    
    * docs: add & use add_perm utility function
    
    * docs: missing spaces
    
    * docs: missing spaces
    
    * feat: grid editor group and permissions (#896)
    
    * feat: grid editor (⚠️ untested)
    
    * fix: grid editor "row" not "rw"
    
    * fix: grid editor permission name typos
    
    * refactor: less calls to `add_perm` (#899)
    
    * refactor: consolidate add_perm calls (⚠️ untested)
    
    * refactor: news_writer (⚠️ untested) & renames
    
    * fix: user lacks [Edit] / [View published] buttons
    
    * fix: errors adding permissions
    
    * docs: CMD-189 groups & permissions & usage
    
    * docs: CMD-189 move group perm descs to files
    
    * refactor: CMD-189 move utils and README to root
    
    * docs: CMD-189 group descs, do not ref other groups
    
    The previous documentation would allow groups that reference other groups to become inaccurate when referenced group changes without updating referencing group
    
    * docs: CMD-189 reduce verbosity of descs
    
    * docs: CMD-189 notes & polish
    
    * docs: CMD-189 clearer group decs
    
    * refactor: CMD-189 allow export of group names
    
    * docs: Development → Update all CMS Instances
    
    Instruct developer how to update all instances of CMS via migrations.
    
    * feat: CMD-189 add standard groups via migration
    
    Testing:
    0. Have a running CMS.
    1. Enter CMS Docker container.
        <sup>e.g. docker exec -it core_cms /bin/bash</sup>
    2. Add groups implicitely via migration:
       `python manage.py migrate taccsite_cms`
    3. Verify [groups], and their permissions, are **present**.
    4. Reverse that migration:
        `python manage.py migrate taccsite_cms zero`
    5. Verify [groups], and their permissions, are **absent**.
    6. Add groups explicitely via migration:
        `python manage.py migrate taccsite_cms 0001_add_groups`
    7. Verify [groups], and their permissions, are **present**.
    8. Reverse that migration:
        `python manage.py migrate taccsite_cms zero`
    9. Verify [groups], and their permissions, are **absent**.
    
    [groups]: https://localhost:8000/admin/auth/group

--- taccsite_cms/management/commands/group_perms/text_editor_advanced.py
+++ taccsite_cms/management/commands/group_perms/text_editor_advanced.py
@@ -1,74 +1,30 @@
-from django.contrib.auth.models import Group, Permission
-from django.contrib.contenttypes.models import ContentType
-from django.core.management import BaseCommand
+"""
+To edit, move, add, and delete textual elements, folders, and files
+"""
+
+from django.contrib.auth.models import Group
+
+from ..util import (
+    let_view_and_change_page_structure,
+    let_view_and_change_text,
+    let_add_and_delete_text,
+    let_view_and_change_folder,
+    let_add_and_delete_folder,
+    let_view_and_change_file,
+    let_add_and_delete_file,
+)
+
+GROUP_NAME = 'Text Editor (Advanced)'
 
 def set_group_perms():
     group, was_created = Group.objects.get_or_create(
-        name='Text Editor (Advanced)'
+        name=GROUP_NAME
     )
 
-    model_name = 'page'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='cms', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can change page', content_type=content_type) )
-    model_name = 'page'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='cms', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can view page', content_type=content_type) )
-
-    model_name = 'placeholder'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='cms', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can use Structure mode', content_type=content_type) )
-
-    model_name = 'link'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='djangocms_link', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can add link', content_type=content_type) )
-    model_name = 'link'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='djangocms_link', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can change link', content_type=content_type) )
-    model_name = 'link'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='djangocms_link', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can delete link', content_type=content_type) )
-    model_name = 'link'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='djangocms_link', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can view link', content_type=content_type) )
-
-    model_name = 'text'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='djangocms_text_ckeditor', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can add text', content_type=content_type) )
-    model_name = 'text'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='djangocms_text_ckeditor', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can change text', content_type=content_type) )
-    model_name = 'text'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='djangocms_text_ckeditor', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can delete text', content_type=content_type) )
-    model_name = 'text'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='djangocms_text_ckeditor', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can view text', content_type=content_type) )
-
-    model_name = 'Folder'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='filer', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can use directory listing', content_type=content_type) )
-    model_name = 'Folder'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='filer', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can add Folder', content_type=content_type) )
-    model_name = 'Folder'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='filer', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can change Folder', content_type=content_type) )
-    model_name = 'Folder'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='filer', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can view Folder', content_type=content_type) )
-    model_name = 'Folder'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='filer', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can delete Folder', content_type=content_type) )
-
-    model_name = 'file'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='filer', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can add file', content_type=content_type) )
-    model_name = 'file'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='filer', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can change file', content_type=content_type) )
-    model_name = 'file'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='filer', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can delete file', content_type=content_type) )
-    model_name = 'file'.lower().replace(' ', '')
-    content_type = ContentType.objects.get(app_label='filer', model=model_name)
-    group.permissions.add( Permission.objects.get(name='Can view file', content_type=content_type) )
+    let_view_and_change_page_structure(group)
+    let_view_and_change_text(group)
+    let_add_and_delete_text(group)
+    let_view_and_change_folder(group)
+    let_add_and_delete_folder(group)
+    let_view_and_change_file(group)
+    let_add_and_delete_file(group)
