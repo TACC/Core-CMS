0e83df93894ebd59768afad4ef3b2b82415caa0d
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-03-03 13:17:49 -0600

    docs: explain perms of renamed util function

--- taccsite_cms/management/commands/util.py
+++ taccsite_cms/management/commands/util.py
@@ -39,6 +39,8 @@ def let_view_page_and_structure(group):
     Add permissions to view a page and its structure
     (Any user that should edit content must also have this permission)
     """
+    # The functional permission of these 'Can … page' is only 'Can change page',
+    # but 'Can view page' makes sense (even though testing suggests it is moot)
     add_perm(group, 'cms', 'page', 'Can view page')
     add_perm(group, 'cms', 'page', 'Can change page')
 
