d3002ba82ee233a5c16ff8b717a9439838f63d17
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-04-09 17:30:19 -0500

    fix(gh-931): cms migrate fails

--- taccsite_cms/migrations/0001_add_groups.py
+++ taccsite_cms/migrations/0001_add_groups.py
@@ -8,6 +8,12 @@ def add_groups(apps, schema_editor):
     from taccsite_cms.management.commands.group_perms.grid_editor_basic import set_group_perms as add_grid_editor_basic
     from taccsite_cms.management.commands.group_perms.grid_editor_advanced import set_group_perms as add_grid_editor_advanced
 
+    # Ensure permissions are created
+    from django.contrib.auth.management import create_permissions
+    from django.apps import apps as django_apps
+    for app_config in django_apps.get_app_configs():
+        create_permissions(app_config, verbosity=0)
+
     add_text_editor_basic()
     add_text_editor_advanced()
     add_media_editor_basic()
@@ -37,6 +43,12 @@ def remove_groups(apps, schema_editor):
     Group.objects.filter(name__in=group_names).delete()
 
 class Migration(migrations.Migration):
+    dependencies = [
+        ('auth', '0001_initial'),
+        ('contenttypes', '0001_initial'),
+        ('cms', '0001_initial'),
+    ]
+
     operations = [
         migrations.RunPython(add_groups, reverse_code=remove_groups),
     ]
