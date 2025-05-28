c0bff6aedff6791ec59cc73a4fd8d4d5ee9dd594
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-05-27 19:10:04 -0500

    feat: let admin change how to open external news

---
+++ taccsite_cms/static/site_cms/js/modules/manageExternalArticles.js
@@ -0,0 +1,46 @@
+/**
+ * Get URL of external web article of which internal news article represents
+ * @param {HTMLElement} article - The article within which to find external URL
+ * @return {string|null} - The URL of the external article
+ */
+export function getArticleExternalURL(article) {
+  if (!article) {
+    console.warn('No article found', article);
+    return;
+  }
+
+  // The admin of news promises leads/abstracts will only have 1 external link
+  const contentLink = article.querySelector('.blog-lead a[href^="http"]');
+  const isExternalLink = contentLink.hostname !== window.location.hostname;
+  const URL = isExternalLink ? contentLink.href : null;
+
+  return URL;
+}
+
+/**
+ * Modify article links to point to external URLs based on tag name
+ * @param {string} [extTagName='external'] - A tag that has been set on articles to select
+ */
+export function changeHowExternalArticleOpens(extTagName = 'external') {
+  const articles = document.querySelectorAll(
+    `.blog-list article.has-blog-tag-${extTagName}`
+  );
+
+  console.debug(`Found ${articles.length} article(s) with tag "${extTagName}"`, articles );
+
+  [ ...articles ].forEach( article => {
+    const externalLinkUrl = getArticleExternalURL( article );
+
+    if ( externalLinkUrl ) {
+      const links = article.querySelectorAll(':is(h3, footer) a');
+
+      // all links to the post should open external URL in new window
+      [ ...links ].forEach( link => {
+        console.debug(`Changed article link to "${externalLinkUrl}"`, link );
+
+        link.href = externalLinkUrl;
+        link.setAttribute('target', '_blank');
+      });
+    }
+  });
+}
