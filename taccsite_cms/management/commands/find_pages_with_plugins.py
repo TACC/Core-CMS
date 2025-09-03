14cb3646a27c378dfc4a4bf5ef3b71e0b9b6b583
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-09-03 18:20:15 -0500

    fix: find_pages_with_plugins
    tested on Texascale prod ad-hoc by copy/pasting and asking AI for help

--- taccsite_cms/management/commands/find_pages_with_plugins.py
+++ taccsite_cms/management/commands/find_pages_with_plugins.py
@@ -1,32 +1,25 @@
-import os
-import sys
-import django
-
-# Configure Django settings to avoid errors
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taccsite_cms.settings')
-django.setup()
-
+from django.core.management import BaseCommand
 from django.apps import apps
 
-def find_page_using_plugin_instance(plugin_instance_id):
-    try:
-        CMSPlugin = apps.get_model('cms', 'CMSPlugin')
-        plugin_instance = CMSPlugin.objects.get(id=plugin_instance_id)
-        page = plugin_instance.placeholder.page if plugin_instance.placeholder else None
-        return page
-    except CMSPlugin.DoesNotExist:
-        return None
+class Command(BaseCommand):
+    help = 'Finds the page that uses a specific plugin instance by its ID'
 
-# Usage example:
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python find_plugin_page.py <plugin_instance_id>")
-        sys.exit(1)
+    def add_arguments(self, parser):
+        parser.add_argument('plugin_instance_ids', nargs='+', type=str)
 
-    plugin_instance_id = int(sys.argv[1])
-    page = find_page_using_plugin_instance(plugin_instance_id)
+    def handle(self, *args, **options):
+        for plugin_instance_id in options['plugin_instance_ids']:
+            page = self.find_page_using_plugin_instance(plugin_instance_id)
+            if page:
+                self.stdout.write(f'Plugin instance ID {plugin_instance_id} is used on page: {page.get_path()}')
+            else:
+                self.stdout.write(f'No page found using plugin instance ID {plugin_instance_id}')
 
-    if page:
-        print(f"The plugin instance with ID {plugin_instance_id} is used on page '{page.get_path()}'")
-    else:
-        print(f"No page uses the plugin instance with ID {plugin_instance_id}")
+    def find_page_using_plugin_instance(self, plugin_instance_id):
+        try:
+            CMSPlugin = apps.get_model('cms', 'CMSPlugin')
+            plugin_instance = CMSPlugin.objects.get(id=plugin_instance_id)
+            page = plugin_instance.placeholder.page if plugin_instance.placeholder else None
+            return page
+        except CMSPlugin.DoesNotExist:
+            return None
