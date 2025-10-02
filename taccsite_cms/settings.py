8f286c31b8a49f7e4abb23e485009bc728d37c36
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-10-02 12:26:34 -0500

    docs: icon picker bug

--- taccsite_cms/settings.py
+++ taccsite_cms/settings.py
@@ -752,6 +752,7 @@ with open(CORTAL_ICONFILE, 'r') as f:
 DJANGOCMS_ICON_SETS = [
     # IMPORTANT: Each set must have same props, even if value is empty
     # WARNING: Values in icon sets are not cleared when user changes set
+    # IMPORTANT: Load 'icon' set last, or selected SVG may be unselected on edit
     # https://github.com/django-cms/djangocms-icon/issues/9
     (LOGO_ICONS, '', _('Logo SVGs')),
     (CORTAL_ICONS, 'icon', _('TACC "Cortal" Icons')),
