8da8e4508389bda7aae683b79be9b03ff4720e3f
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-11-11 21:25:54 -0600

    feat: remove unsupported bootstrap button "size"

--- taccsite_cms/djangocms_bootstrap4/contrib/bootstrap4_link/extend.py
+++ taccsite_cms/djangocms_bootstrap4/contrib/bootstrap4_link/extend.py
@@ -6,8 +6,22 @@ def extendBootstrap4LinkPlugin():
     from cms.plugin_pool import plugin_pool
 
     from djangocms_bootstrap4.contrib.bootstrap4_link.cms_plugins import Bootstrap4LinkPlugin as OriginalBootstrap4LinkPlugin
+    from djangocms_bootstrap4.contrib.bootstrap4_link.models import Bootstrap4Link as OriginalBootstrap4Link
+
+    class Bootstrap4LinkModel(OriginalBootstrap4Link):
+        class Meta:
+            proxy = True
+
+    LINK_SIZE_CHOICES = (
+        ('btn-sm', _('Small')),
+        ('', _('Regular')),
+    )
+
+    OriginalBootstrap4Link._meta.get_field('link_size').choices = LINK_SIZE_CHOICES
 
     class Bootstrap4LinkPlugin(OriginalBootstrap4LinkPlugin):
+        model = Bootstrap4LinkModel
+
         original_fields = OriginalBootstrap4LinkPlugin.fieldsets[0][1]['fields']
         filtered_fields = tuple(f for f in original_fields if f != ('link_outline', 'link_block'))
 
