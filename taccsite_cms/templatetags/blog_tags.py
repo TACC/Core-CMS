26ead03521e9a61d5b72a137a9b1796be1d0f298
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-11-25 11:49:11 -0600

    refactor!: load blog_post_has_ via "blog_tags"
    Put all tgs in one place.
    
    Name them like Django CMS ("cms_tags")

--- taccsite_cms/templatetags/blog_tags.py
+++ taccsite_cms/templatetags/blog_tags.py
@@ -30,3 +30,30 @@ def blog_post_has_category(post=None, category_slug=''):
                 has_category = True
 
     return has_category
+
+@register.simple_tag
+def blog_post_has_tag(post=None, tag_slug=''):
+    """
+    Custom Template Tag `blog_post_has_tag`
+
+    Use: Return (boolean) whether given blog post has a specific tag.
+    Load custom tag into template:
+        {% load blog_post_has_tag %}
+
+    Template inline usage:
+        {# (renders `True` or `False`) #}
+        {% blog_post_has_tag post 'some_tag' %}
+        {# (renders "A" or "B") #}
+        {% blog_post_has_tag post 'some_tag' as is_some_tag %}
+        {% if is_some_tag %} A {% else %} B {% endif %}
+
+    Example:
+        ../templates/djangocms_blog/post_detail.html
+    """
+    has_tag = False
+    if post.tags.exists:
+        for tag in post.tags.all():
+            if tag.slug == tag_slug:
+                has_tag = True
+
+    return has_tag
