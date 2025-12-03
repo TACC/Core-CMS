1a3e0f56c475c24d583f88734f1aad39e2d11b98
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-12-02 16:48:39 -0800

    feat: djangocms-blog v2 for featured posts plugin (#1044)
    * feat: djangocms-blog v2
    
    * fix: djangocms-page-meta template code old & odd
    
    * fix: featured posts plugin
    
    To monkey-patch —
    - https://github.com/nephila/djangocms-blog/pull/782
    - https://github.com/nephila/djangocms-blog/pull/783
    — because I limit dependency update.*
    
    * Upgrading to v2.0.6+ means upgrading django-meta, which leads to bugs unless I upgrade django-filer, which introduces UI bugs.

--- taccsite_cms/apps.py
+++ taccsite_cms/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 
 from .djangocms_bootstrap4.contrib.bootstrap4_link.extend import extendBootstrap4LinkPlugin
+from .djangocms_blog.patch import patchDjangoCMSBlog
 
 logger = logging.getLogger(f"portal.{__name__}")
 
@@ -13,6 +14,7 @@ class TaccsiteCmsConfig(AppConfig):
     def ready(self):
         post_migrate.connect(self.create_groups, sender=self)
         extendBootstrap4LinkPlugin()
+        patchDjangoCMSBlog()
 
     def create_groups(self, sender, **kwargs):
         from django.contrib.auth.models import Group
