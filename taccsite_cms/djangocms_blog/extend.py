373d7ceecaed0f3eaa3245b1ace1af9c8934fdb0
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2026-01-08 13:13:19 -0600

    feat: [CSS] featured posts, flag unpublished posts

--- taccsite_cms/djangocms_blog/extend.py
+++ taccsite_cms/djangocms_blog/extend.py
@@ -1,16 +1,29 @@
 def extendBlogFeaturedPostsPlugin():
     from django import forms
+    from django.utils.html import format_html
     from cms.plugin_pool import plugin_pool
     from djangocms_blog.cms_plugins import BlogFeaturedPostsPlugin as OriginalBlogFeaturedPostsPlugin
     from djangocms_blog.models import FeaturedPostsPlugin
 
     class FeaturedPostsForm(forms.ModelForm):
+        class Media:
+            css = {
+                'all': ('djangocms_blog/css/admin/featured_posts_ordered.css',)
+            }
+
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
             if 'posts' in self.fields:
-                self.fields['posts'].label_from_instance = lambda obj: (
-                    f"✓ {obj.title}" if obj.publish else f"○ {obj.title}"
-                )
+                def label_with_data_attr(obj):
+                    if obj.publish:
+                        return format_html(
+                            '<span data-is-published>{}</span>', obj.title
+                        )
+                    else:
+                        return format_html(
+                            '<span>{}</span>', obj.title
+                        )
+                self.fields['posts'].label_from_instance = label_with_data_attr
 
         class Meta:
             model = FeaturedPostsPlugin
