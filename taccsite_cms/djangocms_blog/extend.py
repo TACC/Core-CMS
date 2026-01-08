a12898df222555b352581c910a02fc132304a29e
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2026-01-08 14:42:45 -0600

    feat: featured posts, show order & flag unpublished posts (#1057)
    ## Overview
    
    - Show order of "Featured Posts".
    - Flag unpublished posts.
    
    ## Related
    
    - [RT
    #37827](https://tickets.tacc.utexas.edu/Ticket/Display.html?id=37827)
    
    ## Changes
    
    - **added** class extension
    - **added** stylesheet
    
    ## Testing
    
    1. Have articles, one unpublished.
    2. List via "Featured Posts".
    3. Edit "Featured Posts".
    4. See clearly which article is unpublished.
    5. See clearly order of articles.
    6. Sort articles; notice ordinal removed until sort is complete.
    7. Filter articles; notice ordinal removed until filter is complete.
    
    ## UI
    
    | before | after |
    | - | - |
    | <img width="755" height="310" alt="before"
    src="https://github.com/user-attachments/assets/2b9361e3-5ef0-46cb-9778-8481b38e922f"
    /> | <img width="755" height="310" alt="after"
    src="https://github.com/user-attachments/assets/2b3f6a8a-f556-4e53-8da7-5ddd2bdce9b2"
    /> |
    
    
    https://github.com/user-attachments/assets/7e6fd3cf-c09a-44f8-8960-bf39168f5741

---
+++ taccsite_cms/djangocms_blog/extend.py
@@ -0,0 +1,67 @@
+def extendBlogFeaturedPostsPlugin():
+    from django import forms
+    from django.utils.html import format_html
+    from django.utils.translation import gettext_lazy as _
+
+    from cms.plugin_pool import plugin_pool
+
+    from djangocms_blog.cms_plugins import BlogFeaturedPostsPlugin as OriginalBlogFeaturedPostsPlugin
+    from djangocms_blog.models import FeaturedPostsPlugin
+
+    class FeaturedPostsForm(forms.ModelForm):
+        class Media:
+            css = {
+                'all': ('djangocms_blog/css/admin/featured_posts_ordered.css',)
+            }
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            if 'posts' in self.fields:
+                def label_with_data_attr(obj):
+                    if obj.publish:
+                        status_value = 'true'
+                        status_text = _('Published')
+                    else:
+                        status_value = 'false'
+                        status_text = _('Unpublished')
+                    return format_html(
+                        '<span data-published="{}">{}</span> {}',
+                        status_value,
+                        status_text,
+                        obj.title
+                    )
+                self.fields['posts'].label_from_instance = label_with_data_attr
+
+        class Meta:
+            model = FeaturedPostsPlugin
+            fields = '__all__'
+
+    class BlogFeaturedPostsPlugin(OriginalBlogFeaturedPostsPlugin):
+        form = FeaturedPostsForm
+
+    plugin_pool.unregister_plugin(OriginalBlogFeaturedPostsPlugin)
+    plugin_pool.register_plugin(BlogFeaturedPostsPlugin)
+
+def extendBlogFeaturedPostsPluginCached():
+    from django import forms
+    from cms.plugin_pool import plugin_pool
+    from djangocms_blog.cms_plugins import BlogFeaturedPostsPluginCached as OriginalBlogFeaturedPostsPluginCached
+    from djangocms_blog.models import FeaturedPostsPlugin
+
+    class FeaturedPostsForm(forms.ModelForm):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            if 'posts' in self.fields:
+                self.fields['posts'].label_from_instance = lambda obj: (
+                    f"✓ {obj.title}" if obj.publish else f"○ {obj.title}"
+                )
+
+        class Meta:
+            model = FeaturedPostsPlugin
+            fields = '__all__'
+
+    class BlogFeaturedPostsPluginCached(OriginalBlogFeaturedPostsPluginCached):
+        form = FeaturedPostsForm
+
+    plugin_pool.unregister_plugin(OriginalBlogFeaturedPostsPluginCached)
+    plugin_pool.register_plugin(BlogFeaturedPostsPluginCached)
