TACC/Core-CMS

commit a12898df222555b352581c910a02fc132304a29e

feat: featured posts, show order & flag unpublished posts (#1057)
## Overview

- Show order of "Featured Posts".
- Flag unpublished posts.

## Related

- [RT
#37827](https://tickets.tacc.utexas.edu/Ticket/Display.html?id=37827)

## Changes

- **added** class extension
- **added** stylesheet

## Testing

1. Have articles, one unpublished.
2. List via "Featured Posts".
3. Edit "Featured Posts".
4. See clearly which article is unpublished.
5. See clearly order of articles.
6. Sort articles; notice ordinal removed until sort is complete.
7. Filter articles; notice ordinal removed until filter is complete.

## UI

| before | after |
| - | - |
| <img width="755" height="310" alt="before"
src="https://github.com/user-attachments/assets/2b9361e3-5ef0-46cb-9778-8481b38e922f"
/> | <img width="755" height="310" alt="after"
src="https://github.com/user-attachments/assets/2b3f6a8a-f556-4e53-8da7-5ddd2bdce9b2"
/> |


https://github.com/user-attachments/assets/7e6fd3cf-c09a-44f8-8960-bf39168f5741

taccsite_cms/djangocms_blog/extend.py
@@ -1,0 +1,67 @@
+def extendBlogFeaturedPostsPlugin():
+    from django import forms
+    from django.utils.html import format_html
+    from django.utils.translation import gettext_lazy as _
+
+    from cms.plugin_pool import plugin_pool
+
+    from djangocms_blog.cms_plugins import BlogFeaturedPostsPlugin as OriginalBlogFeaturedPostsPlugin
+    from djangocms_blog.models import FeaturedPostsPlugin
+
+    class FeaturedPostsForm(forms.ModelForm):
+        class Media:
+            css = {
+                'all': ('djangocms_blog/css/admin/featured_posts_ordered.css',)
+            }
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            if 'posts' in self.fields:
+                def label_with_data_attr(obj):
+                    if obj.publish:
+                        status_value = 'true'
+                        status_text = _('Published')
+                    else:
+                        status_value = 'false'
+                        status_text = _('Unpublished')
+                    return format_html(
+                        '<span data-published="{}">{}</span> {}',
+                        status_value,
+                        status_text,
+                        obj.title
+                    )
+                self.fields['posts'].label_from_instance = label_with_data_attr
+
+        class Meta:
+            model = FeaturedPostsPlugin
+            fields = '__all__'
+
+    class BlogFeaturedPostsPlugin(OriginalBlogFeaturedPostsPlugin):
+        form = FeaturedPostsForm
+
+    plugin_pool.unregister_plugin(OriginalBlogFeaturedPostsPlugin)
+    plugin_pool.register_plugin(BlogFeaturedPostsPlugin)
+
+def extendBlogFeaturedPostsPluginCached():
+    from django import forms
+    from cms.plugin_pool import plugin_pool
+    from djangocms_blog.cms_plugins import BlogFeaturedPostsPluginCached as OriginalBlogFeaturedPostsPluginCached
+    from djangocms_blog.models import FeaturedPostsPlugin
+
+    class FeaturedPostsForm(forms.ModelForm):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            if 'posts' in self.fields:
+                self.fields['posts'].label_from_instance = lambda obj: (
+                    f"✓ {obj.title}" if obj.publish else f"○ {obj.title}"
+                )
+
+        class Meta:
+            model = FeaturedPostsPlugin
+            fields = '__all__'
+
+    class BlogFeaturedPostsPluginCached(OriginalBlogFeaturedPostsPluginCached):
+        form = FeaturedPostsForm
+
+    plugin_pool.unregister_plugin(OriginalBlogFeaturedPostsPluginCached)
+    plugin_pool.register_plugin(BlogFeaturedPostsPluginCached)

taccsite_cms/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 
 from .djangocms_bootstrap4.contrib.bootstrap4_link.extend import extendBootstrap4LinkPlugin
+from .djangocms_blog.extend import extendBlogFeaturedPostsPlugin, extendBlogFeaturedPostsPluginCached
 from .djangocms_blog.patch import patchBlog
 
 logger = logging.getLogger(f"portal.{__name__}")
@@ -14,6 +15,8 @@ class TaccsiteCmsConfig(AppConfig):
     def ready(self):
         post_migrate.connect(self.create_groups, sender=self)
         extendBootstrap4LinkPlugin()
+        extendBlogFeaturedPostsPlugin()
+        extendBlogFeaturedPostsPluginCached()
         patchBlog()
 
     def create_groups(self, sender, **kwargs):

taccsite_cms/static/djangocms_blog/css/admin/featured_posts_ordered.css
@@ -1,0 +1,37 @@
+/* To add numbered ordering to the sortable list */
+.sortedm2m-container ul.sortedm2m {
+    --checkbox-offset: 20px /* from djangocms-admin.css */;
+    --marker-offset: 5px;
+
+    counter-reset: item-counter;
+}
+.sortedm2m-container .sortedm2m li {
+    counter-increment: item-counter;
+    padding-left:
+      var(--marker-offset) !important /* overwrites djangocms-admin.css */;
+}
+.sortedm2m-container ul.sortedm2m label::before {
+    content: counter(item-counter) ".";
+    position: relative;
+    left: calc( -1 * (
+      var(--checkbox-offset)
+      + var(--marker-offset)
+    ) );
+}
+.sortedm2m-container ul.sortedm2m:has(li.ui-sortable-helper) label::before {
+    visibility: hidden;
+}
+
+/* To distinguish published vs unpublished */
+.sortedm2m-container {
+  & [data-published="true"] {
+      display: none;
+  }
+  & label:has([data-published="false"]) {
+      opacity: 0.5;
+  }
+  & [data-published="false"] {
+    &::before { content: '('; }
+    &::after { content: ')'; }
+  }
+}

commit b6b8f8b193fff489ea124123d63e4d469c545654

fix: do not extend nor patch blog if not installed (#1061)
## Overview

Fixes error building instance that has no blog.

## Related

- fixes
[v4.35.20](https://github.com/TACC/Core-CMS/releases/tag/v4.35.20)
- fixes bug introduced by #1057
- similar to #1052

## Changes

- **added** logging to check if `djangocms_blog` is installed before
extending or patching.
- **refactored** installation check to use `is_app_installed()` utility
function for consistency.

## Testing

1. Deploy on website with no blog.
2. See log that patch is skipped.
3. Verify [deploy
succeeds](https://jenkins.portals.tacc.utexas.edu/job/Core_Portal_Deploy/3680/console).

## UI

<img width="900" height="470" alt="Screenshot 2026-01-09 at 15 21 51"
src="https://github.com/user-attachments/assets/786fcd09-a662-4fa9-8980-2f5a0c06063e"
/>

taccsite_cms/djangocms_blog/extend.py
@@ -1,4 +1,14 @@
+import logging
+
+logger = logging.getLogger(f"portal.{__name__}")
+
+from .utils import is_app_installed
+
 def extendBlogFeaturedPostsPlugin():
+    if not is_app_installed():
+        logger.debug('Not extending djangocms_blog, because it is not installed')
+        return
+
     from django import forms
     from django.utils.html import format_html
     from django.utils.translation import gettext_lazy as _
@@ -43,6 +53,10 @@ def extendBlogFeaturedPostsPlugin():
     plugin_pool.register_plugin(BlogFeaturedPostsPlugin)
 
 def extendBlogFeaturedPostsPluginCached():
+    if not is_app_installed():
+        logger.debug('Not extending djangocms_blog, because it is not installed')
+        return
+
     from django import forms
     from cms.plugin_pool import plugin_pool
     from djangocms_blog.cms_plugins import BlogFeaturedPostsPluginCached as OriginalBlogFeaturedPostsPluginCached

taccsite_cms/djangocms_blog/patch.py
@@ -2,12 +2,13 @@ import logging
 
 logger = logging.getLogger(f"portal.{__name__}")
 
+from .utils import is_app_installed
+
 # To monkey-patch nephila/djangocms-blog until v2.0.6+
 # TODO: After upgrading to v2.0.6+, delete this file and its imports
 def patchBlog():
-        from django.apps import apps
-        if not apps.is_installed('djangocms_blog'):
-            logger.debug('Skipping patch of djangocms_blog because it is not installed')
+        if not is_app_installed():
+            logger.debug('Not patching djangocms_blog, because it is not installed')
             return
 
         from djangocms_blog.models import BasePostPlugin, FeaturedPostsPlugin

taccsite_cms/djangocms_blog/utils.py
@@ -1,0 +1,4 @@
+def is_app_installed():
+    """Whether djangocms_blog is installed"""
+    from django.apps import apps
+    return apps.is_installed('djangocms_blog')