ead856928756fab9b40d8746c700b7fc98f11f8f
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2026-02-11 12:52:20 -0600

    refactor: simplify code and format docstring

--- taccsite_cms/templatetags/header_logo_tags.py
+++ taccsite_cms/templatetags/header_logo_tags.py
@@ -7,21 +7,32 @@ register = template.Library()
 @register.simple_tag
 def header_logo_from_picture(instance, picture_link):
     """
-    Build a plugin_logo dict from a djangocms_picture instance for use in header_logo.html.
+    Custom Template Tag `header_logo_from_picture`
 
-    Usage: {% header_logo_from_picture instance picture_link as plugin_logo %}
-    Then:  {% include "header_logo.html" with plugin_logo=plugin_logo %}
+    Use: Build plugin_logo from djangocms_picture instance for header_logo.
+
+    Load custom tag into template:
+        {% load header_logo_from_picture %}
+
+    Template inline usage:
+        {% header_logo_from_picture instance picture_link as plugin_logo %}
+        {% include "header_logo.html" with plugin_logo=plugin_logo %}
     """
-    attributes = getattr(instance, 'attributes', None) or {}
     picture = getattr(instance, 'picture', None)
-    alt = attributes.get('alt') or (getattr(picture, 'default_alt_text', None) if picture else None) or ''
+    attrs = getattr(instance, 'attributes', None) or {}
+
+    default_alt_text = getattr(picture, 'default_alt_text', None) if picture else None
+
+    alt = attrs.get('alt') or default_alt_text or ''
     img_src = getattr(instance, 'img_src', None) or ''
+    link_target = getattr(instance, 'link_target', None)
+
     return {
-        'link_href': picture_link or '#',
-        'link_target': getattr(instance, 'link_target', None) or '',
-        'img_class': '',
+        'is_remote': False,
         'img_file_src': img_src,
-        'is_remote': True,
-        'img_crossorigin': '',
+        'img_class': '',
+        'link_href': picture_link or '/',
+        'link_target': link_target or '',
         'img_alt_text': alt,
+        'img_crossorigin': 'anonymous',
     }
