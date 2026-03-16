89355dd6c2a532528d231c73246c4f1d1c0aa69e
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2026-03-16 14:10:23 -0500

    fix(a11y): new window links override aria-label
    - Introduced a function to ensure a visually hidden span element is present for accessibility.
    - Updated link handling to include `aria-describedby` for links that open in a new tab, enhancing screen reader support.
    - Added constants for better maintainability and clarity in the code.

--- taccsite_cms/static/site_cms/js/modules/setTargetForExternalLinks.js
+++ taccsite_cms/static/site_cms/js/modules/setTargetForExternalLinks.js
@@ -4,6 +4,29 @@
  */
 export const DEFAULT_LINKS = document.querySelectorAll('body > :is(header, main, footer) a');
 
+// IDEA: Could become `opts` in `findLinksAndSetTargets` e.g.
+// - opts.idOpenNewWindow
+// - opts.textOpenNewWindow
+// - opts.classVisuallyHidden
+const ID_OPEN_NEW_WINDOW = 'msg-open-new-window';
+const TEXT_OPEN_NEW_WINDOW = 'Opens in a new window';
+const CLASS_VISUALLY_HIDDEN = 'sr-only';
+
+/**
+ * Ensure document has visually hidden message for aria-describedby
+ * (Creates such an element once if missing)
+ */
+function ensureOpenNewWindowSpan() {
+  if ( document.getElementById( ID_OPEN_NEW_WINDOW ) ) {
+    return;
+  }
+  const span = document.createElement( 'span' );
+  span.id = ID_OPEN_NEW_WINDOW;
+  span.className = CLASS_VISUALLY_HIDDEN;
+  span.textContent = TEXT_OPEN_NEW_WINDOW;
+  document.body.appendChild( span );
+}
+
 /**
  * Make links with absolute URLs open in new tab, and:
  * - add accessible markup
@@ -19,6 +42,8 @@ export default function findLinksAndSetTargets( links = DEFAULT_LINKS, opts = {
 }) {
   const baseDocHost = document.location.host;
   const baseDocHostWithSubdomain= `www.${ baseDocHost }`;
+
+  ensureOpenNewWindowSpan();
 
   if ( opts.shouldDebug ) {
     console.log({ links, opts });
@@ -49,7 +74,16 @@ export default function findLinksAndSetTargets( links = DEFAULT_LINKS, opts = {
         }
       }
       if ( link.target === '_blank') {
-        link.setAttribute('aria-label', 'Opens in new window.');
+        const hasA11yNote = link.querySelector( 'span.' + CLASS_VISUALLY_HIDDEN );
+        const hasA11yDesc = link.hasAttribute('aria-describedby');
+
+        if ( ! hasA11yNote && ! hasA11yDesc ) {
+          link.setAttribute( 'aria-describedby', ID_OPEN_NEW_WINDOW );
+
+          if ( opts.shouldDebug ) {
+            console.debug(`Link "${ linkHref }" now has "aria-describedby"`);
+          }
+        }
       }
     }
 
