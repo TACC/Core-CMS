68eb3e250c0a33ff2544d53009072a97902772d9
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2026-05-22 18:45:57 -0500

    fix(ckeditor): pastefromgdocs bold/italic are span

--- taccsite_cms/static/js/addons/ckeditor.wysiwyg.js
+++ taccsite_cms/static/js/addons/ckeditor.wysiwyg.js
@@ -7,6 +7,35 @@ CKEDITOR.plugins.addExternal(
   'plugin.js'
 );
 
+/* Convert inline bold/italic styles to semantic tags on paste,
+   because the site strips inline styles from saved content,
+   yet `pastefromgdocs` bold/italic are as inline styles. */
+CKEDITOR.on('instanceReady', function(ev) {
+  ev.editor.on('paste', function(e) {
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(e.data.dataValue, 'text/html');
+
+    doc.querySelectorAll('span').forEach(function(span) {
+      var fw = span.style.fontWeight;
+      var fs = span.style.fontStyle;
+      var replacement;
+
+      if (fw === 'bold' || fw === '700') {
+        replacement = doc.createElement('strong');
+      } else if (fs === 'italic') {
+        replacement = doc.createElement('em');
+      }
+
+      if (replacement) {
+        while (span.firstChild) replacement.appendChild(span.firstChild);
+        span.parentNode.replaceChild(replacement, span);
+      }
+    });
+
+    e.data.dataValue = doc.body.innerHTML;
+  }, null, null, 1);
+});
+
 CKEDITOR.stylesSet.add( 'default', [
 
 	/* Block styles */
