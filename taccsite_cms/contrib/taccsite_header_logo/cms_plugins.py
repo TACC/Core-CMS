5d3f83ed08eafedbbfc112821436eade84365b93
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2026-06-10 18:31:31 -0500

    refactor(header): remove cruft

--- taccsite_cms/contrib/taccsite_header_logo/cms_plugins.py
+++ taccsite_cms/contrib/taccsite_header_logo/cms_plugins.py
@@ -4,54 +4,23 @@ from django.utils.translation import gettext_lazy as _
 from djangocms_picture.cms_plugins import PicturePlugin
 from djangocms_picture.models import Picture
 
+HEADER_LOGO_ELEMENT_ID = 'header-logo'
+
 
 @plugin_pool.register_plugin
 class HeaderLogoPlugin(PicturePlugin):
     """
     Header > "Header logo" plugin
 
-    Same fields as Picture; fixed navbar markup (no template dropdown).
+    Full Picture plugin; default id="header-logo" when not set in Attributes.
     """
     model = Picture
     module = _('TACC Header')
     name = _('Header logo')
-    render_template = 'taccsite_header_logo/header_logo_plugin.html'
-    cache = False
 
-    fieldsets = [
-        (None, {
-            'fields': (
-                'picture',
-                'external_picture',
-            )
-        }),
-        (_('Advanced settings'), {
-            'classes': ('collapse',),
-            'fields': (
-                'use_responsive_image',
-                ('width', 'height'),
-                'alignment',
-                'caption_text',
-                'attributes',
-            )
-        }),
-        (_('Link settings'), {
-            'classes': ('collapse',),
-            'fields': (
-                ('link_url', 'link_page'),
-                'link_target',
-                'link_attributes',
-            )
-        }),
-        (_('Cropping settings'), {
-            'classes': ('collapse',),
-            'fields': (
-                ('use_automatic_scaling', 'use_no_cropping'),
-                ('use_crop', 'use_upscale'),
-                'thumbnail_options',
-            )
-        })
-    ]
-
-    def get_render_template(self, context, instance, placeholder):
-        return self.render_template
+    def render(self, context, instance, placeholder):
+        if not instance.attributes:
+            instance.attributes = {}
+        if 'id' not in instance.attributes:
+            instance.attributes['id'] = HEADER_LOGO_ELEMENT_ID
+        return super().render(context, instance, placeholder)
--- taccsite_cms/templatetags/header_content_tags.py
+++ taccsite_cms/templatetags/header_content_tags.py
@@ -38,25 +38,3 @@ def render_header_logo_plugin_html(request, context):
 def render_header_logo_plugin(context):
     html = render_header_logo_plugin_html(context['request'], context)
     return mark_safe(html) if html else ''
-
-
-@register.simple_tag
-def header_logo_from_picture(instance, picture_link):
-    """
-    Build plugin_logo dict from djangocms_picture Picture instance for header markup.
-    """
-    picture = getattr(instance, 'picture', None)
-    attrs = getattr(instance, 'attributes', None) or {}
-
-    default_alt_text = getattr(picture, 'default_alt_text', None) if picture else None
-
-    alt = attrs.get('alt') or default_alt_text or ''
-    img_src = getattr(instance, 'img_src', None) or ''
-    link_target = getattr(instance, 'link_target', None)
-
-    return {
-        'img_file_src': img_src,
-        'link_href': picture_link or '/',
-        'link_target': link_target or '',
-        'img_alt_text': alt,
-    }
