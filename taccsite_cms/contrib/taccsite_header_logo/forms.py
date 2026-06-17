9e45e6382a705b609c604888a115f3d043b73c46
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2026-06-16 19:00:50 -0500

    feat(gh-999): Portal logo picture template for header img class
    Register Portal logo template and picture_attributes_img on default picture; default Header logo to portal_logo with mr-5 on link attributes.

--- taccsite_cms/contrib/taccsite_header_logo/forms.py
+++ taccsite_cms/contrib/taccsite_header_logo/forms.py
@@ -2,6 +2,8 @@ from cms.models import Page
 from django.contrib.sites.models import Site
 from djangocms_picture.forms import PictureForm
 
+HEADER_LOGO_PICTURE_TEMPLATE = 'portal_logo'
+
 
 def default_home_page():
     site = Site.objects.get_current()
@@ -11,12 +13,17 @@ def default_home_page():
 class HeaderLogoForm(PictureForm):
     """
     Defaults for new Header logo plugins only (admin add form).
+
+    Uses Portal logo picture template (portal-logo on img). Attributes class
+    applies to the link when a link is set (see default picture.html).
     """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if self.instance.pk:
             return
+
+        self.initial.setdefault('template', HEADER_LOGO_PICTURE_TEMPLATE)
 
         home = default_home_page()
         if home is not None:
@@ -25,3 +32,6 @@ class HeaderLogoForm(PictureForm):
         self.initial.setdefault('use_no_cropping', True)
         self.initial.setdefault('use_automatic_scaling', False)
 
+        attributes = dict(self.initial.get('attributes') or {})
+        attributes.setdefault('class', 'mr-5')
+        self.initial['attributes'] = attributes
