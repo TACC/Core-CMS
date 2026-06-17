949b2356e8b268751703aa418d130ef64a354e76
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2026-06-16 19:17:13 -0500

    refactor(HeaderLogoForm): update template initialization and remove default attributes
    - Added conditional logic to set the template based on the PORTAL_IS_TACC_CORE_PORTAL setting.
    - Removed the default 'mr-5' class from the attributes in the HeaderLogoForm.
    - Updated the body class in base.html to remove the conditional portal class.

--- taccsite_cms/contrib/taccsite_header_logo/forms.py
+++ taccsite_cms/contrib/taccsite_header_logo/forms.py
@@ -1,4 +1,5 @@
 from cms.models import Page
+from django.conf import settings
 from django.contrib.sites.models import Site
 from djangocms_picture.forms import PictureForm
 
@@ -14,8 +15,7 @@ class HeaderLogoForm(PictureForm):
     """
     Defaults for new Header logo plugins only (admin add form).
 
-    Uses Portal logo picture template (portal-logo on img). Attributes class
-    applies to the link when a link is set (see default picture.html).
+    On a TACC/Core-Portal instance, defaults to Portal logo picture template.
     """
 
     def __init__(self, *args, **kwargs):
@@ -23,7 +23,8 @@ class HeaderLogoForm(PictureForm):
         if self.instance.pk:
             return
 
-        self.initial.setdefault('template', HEADER_LOGO_PICTURE_TEMPLATE)
+        if settings.PORTAL_IS_TACC_CORE_PORTAL:
+            self.initial.setdefault('template', HEADER_LOGO_PICTURE_TEMPLATE)
 
         home = default_home_page()
         if home is not None:
@@ -31,7 +32,3 @@ class HeaderLogoForm(PictureForm):
 
         self.initial.setdefault('use_no_cropping', True)
         self.initial.setdefault('use_automatic_scaling', False)
-
-        attributes = dict(self.initial.get('attributes') or {})
-        attributes.setdefault('class', 'mr-5')
-        self.initial['attributes'] = attributes
