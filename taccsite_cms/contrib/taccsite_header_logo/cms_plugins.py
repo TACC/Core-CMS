559dbf7f45bc7fa773a87e78a8720b475f3bb6d3
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2026-06-18 17:13:38 -0500

    fix(GH-1185): apply portal-logo class for img-only header logos
    HeaderLogoPlugin mirrors picture template hoist rules so portal-logo
    lands on img with a single class attribute when attrs are not hoisted,
    and via picture_img_class when they are.
    
    Closes #1185

--- taccsite_cms/contrib/taccsite_header_logo/cms_plugins.py
+++ taccsite_cms/contrib/taccsite_header_logo/cms_plugins.py
@@ -7,6 +7,34 @@ from djangocms_picture.models import Picture
 from .forms import HeaderLogoForm
 
 HEADER_LOGO_LINK_CLASS = 'navbar-brand'
+PORTAL_LOGO_IMG_CLASS = 'portal-logo'
+
+
+def picture_attributes_hoist_to_parent(instance, picture_link):
+    """Match djangocms_picture/default/picture.html attribute placement."""
+    default_caption = ''
+    if instance.picture:
+        default_caption = getattr(instance.picture, 'default_caption', '') or ''
+    return bool(
+        instance.caption_text
+        or default_caption
+        or picture_link
+        or instance.child_plugin_instances
+    )
+
+
+def apply_header_logo_classes(instance, context, picture_link):
+    if not instance.attributes:
+        instance.attributes = {}
+
+    existing_class = instance.attributes.get('class', '')
+    link_class = f'{HEADER_LOGO_LINK_CLASS} {existing_class}'.strip()
+
+    if picture_attributes_hoist_to_parent(instance, picture_link):
+        instance.attributes['class'] = link_class
+        context['picture_img_class'] = PORTAL_LOGO_IMG_CLASS
+    else:
+        instance.attributes['class'] = f'{link_class} {PORTAL_LOGO_IMG_CLASS}'.strip()
 
 
 @plugin_pool.register_plugin
@@ -20,11 +48,5 @@ class HeaderLogoPlugin(PicturePlugin):
     name = _('Header logo')
 
     def render(self, context, instance, placeholder):
-        if not instance.attributes:
-            instance.attributes = {}
-
-        # To add required attributes
-        existing_class = instance.attributes.get('class', '')
-        instance.attributes['class'] = f'{HEADER_LOGO_LINK_CLASS} {existing_class}'.strip()
-
+        apply_header_logo_classes(instance, context, instance.get_link())
         return super().render(context, instance, placeholder)
